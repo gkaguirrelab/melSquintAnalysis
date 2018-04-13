aaa2c79bf4e2f2e7a09aee57b1726ea79a5013e8
gkaguirre <gkaguirre@me.com>
2018-04-13 11:39:13 -0400

    Setting up TbTb config
    Signed-off-by: gkaguirre <gkaguirre@me.com>

---
+++ config/melSquintAnalysisLocalHookTemplate.m
@@ -0,0 +1,67 @@
+function melSquintAnalysisLocalHook
+%  melSquintAnalysisLocalHook
+%
+% Configure things for working on the  melSquintAnalysis project.
+%
+% For use with the ToolboxToolbox.
+%
+% If you 'git clone' melSquintAnalysis into your ToolboxToolbox "projectRoot"
+% folder, then run in MATLAB
+%   tbUseProject('melSquintAnalysis')
+% ToolboxToolbox will set up melSquintAnalysis and its dependencies on
+% your machine.
+%
+% As part of the setup process, ToolboxToolbox will copy this file to your
+% ToolboxToolbox localToolboxHooks directory (minus the "Template" suffix).
+% The defalt location for this would be
+%   ~/localToolboxHooks/melSquintAnalysisLocalHook.m
+%
+% Each time you run tbUseProject('melSquintAnalysis'), ToolboxToolbox will
+% execute your local copy of this file to do setup for melSquintAnalysis.
+%
+% You should edit your local copy with values that are correct for your
+% local machine, for example the output directory location.
+%
+
+
+%% Say hello.
+fprintf('melSquintAnalysis local hook.\n');
+projectName = 'melSquintAnalysis';
+
+%% Delete any old prefs
+if (ispref(projectName))
+    rmpref(projectName);
+end
+
+%% Specify base paths for materials and data
+[~, userID] = system('whoami');
+userID = strtrim(userID);
+switch userID
+    case {'melanopsin' 'pupillab'}
+        materialsBasePath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/MELA_materials'];
+        MELA_dataBasePath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/MELA_data/'];
+    case {'dhb'}
+        materialsBasePath = ['/Users1' '/Dropbox (Aguirre-Brainard Lab)/MELA_materials'];
+        MELA_dataBasePath = ['/Users1' '/Dropbox (Aguirre-Brainard Lab)/MELA_data/'];     
+    case {'mbarnett'}
+        materialsBasePath = ['/home/mbarnett/Dropbox (Aguirre-Brainard Lab)/MELA_materials'];
+        MELA_dataBasePath = ['/home/mbarnett/Dropbox (Aguirre-Brainard Lab)/MELA_data/'];
+    otherwise
+        materialsBasePath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/MELA_materials'];
+        MELA_dataBasePath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/MELA_data/'];
+end
+
+%% Specify where output goes
+
+if ismac
+    % Code to run on Mac plaform
+    setpref(projectName,'melaDataPath', MELA_dataBasePath);
+elseif isunix
+    % Code to run on Linux plaform
+    setpref(projectName,'melaDataPath', MELA_dataBasePath);
+elseif ispc
+    % Code to run on Windows platform
+    warning('No supported for PC')
+else
+    disp('What are you using?')
+end
