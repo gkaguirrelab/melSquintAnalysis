1c2bda506f0b02b1a5d975ee6f16f4c372dd9f65
harrison-mcadams <harrisonmcadams@huntsman-ve502-0249.apn.wlan.upenn.edu>
2018-04-18 13:59:02 -0400

    up and running
    the pupilPipelineWrapper function can now attempt to analyze all videos
    associated with a given session, on the basis of some basic subject
    information provided by trackingScript

--- code/trackingScript.m
+++ code/trackingScript.m
@@ -5,17 +5,17 @@
 
 % get data and analysis directory prefs
 projectName = 'melSquintAnalysis';
-MELA_dataBasePath =  getpref(projectName,'melaDataPath');
-MELA_analysisBasePath = getpref(projectName,'melaAnalysisPath');
+pathParams.dataBasePath =  getpref(projectName,'melaDataPath');
+pathParams.analysisBasePath = getpref(projectName,'melaAnalysisPath');
 
 
 
 %% Parameters common to all subjects
-pathParams.intrinsicCameraMatrix =  [1347.76, 0, 658.90;...
+cameraParams.intrinsicCameraMatrix =  [1347.76, 0, 658.90;...
                           0, 1345.48, 365.68;...
                           0, 0, 1];
-pathParams.sensorResolution = [1280 720];      
-pathParams.radialDistortionVector = [0.21524, -1.5616];
+cameraParams.sensorResolution = [1280 720];      
+cameraParams.radialDistortionVector = [0.21524, -1.5616];
 pathParams.verbose = true;
 pathParams.useParallel = true;
 
@@ -24,56 +24,23 @@ pathParams.useParallel = true;
 
 %% MELA_0124
 pathParams.subject = 'MELA_0124';
-pathParams.protocol = 
+pathParams.protocol = 'Screening';
 pathParams.session = '2018-04-12_session_1';
-pathParams.runName = 'calibration_002_MELA_0124';
-eyeLaterality = 'left';
-observedIrisDiamPixels = 550;
+pathParams.eyeLaterality = 'left';
 
-% Estimate camera depth from iris diameter
-sceneGeometry = createSceneGeometry(...
-    'intrinsicCameraMatrix', intrinsicCameraMatrix, ...
-    'radialDistortionVector',radialDistortionVector);
-%[cameraDepthMean, cameraDepthSD] = depthFromIrisDiameter( sceneGeometry, observedIrisDiamPixels );
-cameraDepthMean = load(fullfile(basePathParams.dataSourceDirFull, subject, session, 'pupilCalibration', 'distance.mat'));
-%cameraDepthMean = load('/Users/harrisonmcadams/Dropbox (Aguirre-Brainard Lab)/MELA_data/Experiments/OLApproach_Squint/Screening/DataFiles/MELA_0124/2018-04-12_session_1/pupilCalibration/distance.mat');
+
+cameraDepthMean = load(fullfile(pathParams.dataBasePath, 'Experiments/OLApproach_Squint', pathParams.protocol, 'DataFiles', pathParams.subject, pathParams.session, 'pupilCalibration', 'distance.mat'));
 cameraDepthMean = cameraDepthMean.distanceFromCornealApexToIRLens;
-cameraDepthSD = 1.4;
+cameraDepthSD = 1.4; % just a value on the order of what depthFromIrisDiameter would provide
 
 
 % Set up scene parameter bounds
-sceneParamsLB = [-15; 1; -5; cameraDepthMean-2*cameraDepthSD; .75; 0.9];
-sceneParamsLBp = [-12; 1.5; -4; cameraDepthMean-1*cameraDepthSD; .85; 0.95];
-sceneParamsUBp = [-8; 2; -3; cameraDepthMean+1*cameraDepthSD; 1.15; 1.05 ];
-sceneParamsUB = [-5; 2.5; -2; cameraDepthMean+2*cameraDepthSD; 1.25; 1.10];
+sceneParams.LB = [-15; 1; -5; cameraDepthMean-2*cameraDepthSD; .75; 0.9];
+sceneParams.LBp = [-12; 1.5; -4; cameraDepthMean-1*cameraDepthSD; .85; 0.95];
+sceneParams.UBp = [-8; 2; -3; cameraDepthMean+1*cameraDepthSD; 1.15; 1.05 ];
+sceneParams.UB = [-5; 2.5; -2; cameraDepthMean+2*cameraDepthSD; 1.25; 1.10];
 
-% Set up path params for this video
-pathParams.dataSourceDirFull = fullfile(basePathParams.dataSourceDirFull, subject, session, 'pupilCalibration');
-pathParams.dataOutputDirFull = fullfile(basePathParams.dataOutputDirFull, subject, session, 'pupilCalibration');
-pathParams.runName = runName;
-
-% Explicitly define the "gray video" name, as this has a different than
-% typically expected suffix
-pathParams.grayVideoName = fullfile(basePathParams.dataSourceDirFull, subject, session, 'pupilCalibration',[pathParams.runName '.mp4']);
-
-
-% Run the video pipeline for the calibration video
-runVideoPipeline(pathParams,...
-    'skipStageByNumber',[1],...
-    'useParallel', useParallel,...
-    'verbose', verbose, ...
-    'glintFrameMask',[180 340 350 500],'glintGammaCorrection', 15, 'numberOfGlints', 2, ...
-    'pupilRange', [60 200],'pupilFrameMask', [100 400 240 300],'pupilCircleThresh', 0.02,'pupilGammaCorrection', 0.7,'maskBox', [1 1],...
-    'cutErrorThreshold', 10, 'badFrameErrorThreshold', 6,'glintPatchRadius', 35, 'ellipseTransparentUB',[1280,720,20000,0.6,pi], ...
-    'sceneParamsLB',sceneParamsLB, 'sceneParamsUB',sceneParamsUB, ...
-    'sceneParamsLBp',sceneParamsLBp,'sceneParamsUBp',sceneParamsUBp,...
-    'intrinsicCameraMatrix', intrinsicCameraMatrix, ...
-    'sensorResolution', sensorResolution, ...
-    'radialDistortionVector',radialDistortionVector, ...
-    'constraintTolerance',0.03, ...
-    'eyeLaterality',eyeLaterality, ...
-    'makeFitVideoByNumber',[3 6 8]);
-
+pupilPipelineWrapper(pathParams, sceneParams, cameraParams);
 
 %% MELA_0125
 pathParams.subject = 'MELA_0125';
