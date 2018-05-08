a9eeb8118127afa09ff41ca12bd15230e31229e0
harrison-mcadams <harrison.mcadams@gmail.com>
2018-05-08 16:27:32 -0400

    up and running
    attempts to identify bad frames, which are defined as: poor ellipse fits, duplicate frames, or NaN values (like when the code has identified the frame contains a blink)

--- code/analyzeScreening.m
+++ code/analyzeScreening.m
@@ -1,5 +1,7 @@
 function [ passStatus, percentGoodFramesPerTrial ] = analyzeScreening(subjectID)
 
+
+threshold = 5;
 %% find the relevant DataFiles dir
 projectName = 'melSquintAnalysis';
 dataDirBase = fullfile(getpref(projectName, 'melaAnalysisPath'), 'Experiments/OLApproach_Squint/Screening/DataFiles');
@@ -23,28 +25,47 @@ dataDir = fullfile(dataDirBase, subjectID, sessionID, 'videoFiles_acquisition_01
 nTrials = 12;
 figure;
 for tt = 1:nTrials
+    
+    % load the trial data
     trialDataStruct = load(fullfile(dataDir, sprintf('trial_%03d_pupil.mat',tt)));
     trialData = trialDataStruct.pupilData.initial.ellipses.values(:, 3);
-    numberBadFrames = sum(isnan(trialData));
-    goodFramesPercentage = (length(trialData) - numberBadFrames)/length(trialData);
-    percentGoodFramesPerTrial(tt) = goodFramesPercentage;
     
+    % plot the RMSE
     subplot(2,6,tt)
     title(sprintf('Trial %0d', tt))
     plot(trialDataStruct.pupilData.initial.ellipses.RMSE)
     
+    % identify and report poor fit frames
+    % poot fit frames here are defined as RMSE of the fit between the
+    % ellipse and pupil perimeter greater than some threshold (5 seems
+    % reasonable looking at the data)
+    poorFitFramesIndices = find(trialDataStruct.pupilData.initial.ellipses.RMSE > threshold);
+    
+    if length(poorFitFramesIndices) ~= 0
+        fprintf([fprintf('Trial %0d: %0d poor fit frames at ', tt, length(poorFitFramesIndices)), mat2str(poorFitFramesIndices) '\n']);
+    end
+    
+    
+    % identify and report duplicate frames
+    % duplicate frames are identified as when the consecutive frame's RMSE
+    % has not changed
     differential = diff(trialDataStruct.pupilData.initial.ellipses.RMSE);
     duplicateFrameIndices = find(differential == 0);
     
     if length(duplicateFrameIndices) ~= 0
         fprintf([fprintf('Trial %0d: %0d duplicate frames at ', tt, length(duplicateFrameIndices)), mat2str(duplicateFrameIndices) '\n']);
     end
-        
     
-
+    % determine how many frames are "bad". bad frames include: no ellipse
+    % (blink), poor ellipse fit (RMSE > threshold), and duplicate frames
+    numberBadFrames = sum(isnan(trialData)) + length(poorFitFramesIndices) + length(duplicateFrameIndices);
+    goodFramesPercentage = (length(trialData) - numberBadFrames)/length(trialData);
+    percentGoodFramesPerTrial(tt) = goodFramesPercentage;
     
-
-
+    
+    
+    
+    
     
     
 end
@@ -59,6 +80,7 @@ percentageOfGoodFramesWithinTrialCriteria = 0.75;
 % percentage of total trials which must be good
 percentageOfGoodTrialsCriteria = 0.75;
 
+% loop through the trials, determine how many of them are good
 nGoodTrials = 0;
 for tt = 1:nTrials
     if percentGoodFramesPerTrial(tt) >= percentageOfGoodFramesWithinTrialCriteria
@@ -66,6 +88,7 @@ for tt = 1:nTrials
     end
 end
 
+% see if enough trials are good so that we avoid exclusion criteria
 if nGoodTrials/nTrials >= percentageOfGoodTrialsCriteria
     passStatus = 1;
 else
