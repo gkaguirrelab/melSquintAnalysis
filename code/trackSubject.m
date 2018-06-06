b97427400617b0943fcec5266ca634877ded3740
harrison-mcadams <harrison.mcadams@gmail.com>
2018-06-06 17:42:03 -0400

    new function to run single subjects
    from start to finish, including the ability to guess initial parameters

---
+++ code/trackSubject.m
@@ -0,0 +1,48 @@
+function trackSubject(subjectID, sessionID, varargin)
+
+%% Input parser
+p = inputParser; p.KeepUnmatched = true;
+
+p.addParameter('approach', 'Squint' ,@isstr);
+p.addParameter('protocol', 'SquintToPulse' ,@isstr);
+
+p.parse(varargin{:})
+
+% grab generic info
+[ fitParams, cameraParams, pathParams ] = getDefaultParams(varargin{:});
+
+
+pathParams.subject = subjectID;
+pathParams.protocol = p.Results.protocol;
+pathParams.session = sessionID;
+
+cameraDepthMean = load(fullfile(pathParams.dataBasePath, 'Experiments/OLApproach_Squint', pathParams.protocol, 'DataFiles', pathParams.subject, pathParams.session, 'pupilCalibration', 'distance.mat'));
+cameraDepthMean = cameraDepthMean.distanceFromCornealApexToIRLens;
+cameraDepthSD = 1.4; % just a value on the order of what depthFromIrisDiameter would provide
+
+
+% Set up scene parameter bounds
+sceneParams.LB = [-15; 1; -5; cameraDepthMean-2*cameraDepthSD; .75; 0.9];
+sceneParams.LBp = [-12; 1.5; -4; cameraDepthMean-1*cameraDepthSD; .85; 0.95];
+sceneParams.UBp = [-8; 2; -3; cameraDepthMean+1*cameraDepthSD; 1.15; 1.05 ];
+sceneParams.UB = [-5; 2.5; -2; cameraDepthMean+2*cameraDepthSD; 1.25; 1.10];
+
+% grab trial list
+[runNamesList, subfoldersList] = getTrialList(pathParams, varargin{:});
+
+grayVideoName = fullfile(pathParams.dataSourceDirFull, pathParams.subject, pathParams.session, subfoldersList{1}, runNamesList{1});
+
+[initialParams] = transparentTrackGUI(grayVideoName, varargin{:});
+% incorporate new initialParams
+fitParams.pupilFrameMask = initialParams.pupilFrameMask;
+fitParams.pupilRange = initialParams.pupilRange;
+fitParams.glintFrameMask = initialParams.glintFrameMask;
+fitParams.pupilCircleThresh = initialParams.pupilCircleThresh;
+
+% do the tracking
+pupilPipelineWrapper(pathParams, sceneParams, cameraParams, fitParams);
+
+
+
+end
+
