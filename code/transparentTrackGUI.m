a703dd085dd38247f45191f7eb08adfeb41709d5
harrison-mcadams <harrison.mcadams@gmail.com>
2018-06-07 14:35:02 -0400

    tweaks for user
    -verbose flag to show what initialParams we came up with
    -text on the plot to tell the user what they're supposed to do
    -console will ask if we're satisfied or need to redo initial placement

--- code/transparentTrackGUI.m
+++ code/transparentTrackGUI.m
@@ -1,4 +1,4 @@
-function [ initialParams ] = transparentTrackGUI(grayVideoName, varargin)
+function [ initialParams ] = transparentTrackGUI(grayVideoName, defaultParams, varargin)
 
 %% Input parser
 p = inputParser; p.KeepUnmatched = true;
@@ -10,27 +10,59 @@ p.addRequired('grayVideoName',@isstr);
 % Optional flow control params
 p.addParameter('frameNumber',1,@isnumeric);
 p.addParameter('openVideo',true,@islogical);
+p.addParameter('verbose',true,@islogical);
 
 
 
-% Optional analysis params
-
+% automatic default parameters in case no default parameters are provided
 p.addParameter('ellipseTransparentUB', [1280, 720, 90000, 0.6, pi], @isnumeric);
 p.addParameter('ellipseTransparentLB', [0, 0, 1000, 0, 0], @isnumeric);
-p.addParameter('pupilMaskShrinkFactor', 0.9, @isnumeric);
-p.addParameter('pupilMaskDilationFactor', 4, @isnumeric);
-p.addParameter('pupilRangeDilator', 1.1, @isnumeric);
-p.addParameter('pupilRangeContractor', 0.9, @isnumeric);
-p.addParameter('innerDilationFactor', 1.1, @isnumeric);
-p.addParameter('outerDilationFactor', 1.3, @isnumeric);
 p.addParameter('pupilGammaCorrection', 0.75, @isnumeric);
 p.addParameter('frameMaskValue', 220, @isnumeric);
+
+% parameters to
+p.addParameter('pupilMaskShrinkFactor', 0.9, @isnumeric);
+p.addParameter('pupilMaskDilationFactor', 4, @isnumeric);
+p.addParameter('pupilRangeDilator', 1.2, @isnumeric);
+p.addParameter('pupilRangeContractor', 0.8, @isnumeric);
+p.addParameter('innerDilationFactor', 1.1, @isnumeric);
+p.addParameter('outerDilationFactor', 1.3, @isnumeric);
 p.addParameter('potentialThreshValues', [0.001:0.001:0.2], @isnumeric);
 p.addParameter('intensityDividerComputeMethod', 'irisMaskMinimum', @isstr);
 p.addParameter('glintMaskPaddingFactor', 50, @isnumeric);
 
 % parse
 p.parse(grayVideoName, varargin{:})
+
+if ~isfield(defaultParams, 'ellipseTransparentUB')
+    ellipseTransparentUB = p.Results.ellipseTransparentUB;
+else
+    ellipseTransparentUB = defaultParams.ellipseTransparentUB;
+end
+
+if ~isfield(defaultParams, 'ellipseTransparentLB')
+    ellipseTransparentLB = p.Results.ellipseTransparentLB;
+else
+    ellipseTransparentLB = defaultParams.ellipseTransparentLB;
+end
+
+if ~isfield(defaultParams, 'pupilGammaCorrection')
+    pupilGammaCorrection = p.Results.pupilGammaCorrection;
+else
+    pupilGammaCorrection = defaultParams.pupilGammaCorrection;
+end
+
+if ~isfield(defaultParams, 'frameMaskValue')
+    frameMaskValue = p.Results.frameMaskValue;
+else
+    frameMaskValue = defaultParams.frameMaskValue;
+end
+
+if ~isfield(defaultParams, 'numberOfGlints')
+    numberOfGlints = p.Results.numberOfGlints;
+else
+    numberOfGlints = defaultParams.numberOfGlints;
+end
 
 
 %% Load up the frame of interest
@@ -44,9 +76,9 @@ grayVideo = zeros(videoSizeY,videoSizeX,nFrames,'uint8');
 %thisFrame = readFrame(videoInObj);
 %thisFrame = squeeze(thisFrame);
 for ii = 1:nFrames
-        thisFrame = readFrame(videoInObj);
-%        thisFrame = imadjust(thisFrame,[],[],p.Results.glintGammaCorrection);
-        grayVideo(:,:,ii) = rgb2gray (thisFrame);
+    thisFrame = readFrame(videoInObj);
+    %        thisFrame = imadjust(thisFrame,[],[],p.Results.glintGammaCorrection);
+    grayVideo(:,:,ii) = rgb2gray (thisFrame);
 end
 
 thisFrame = squeeze(grayVideo(:,:,p.Results.frameNumber));
@@ -64,10 +96,14 @@ hold on
 
 %% Guess initial pupil position
 % begin user input
+fprintf('Define pupil boundary in figure.\n')
+string = 'Define the pupil boundary by clicking along the pupil perimeter. Press ENTER when finished.';
+hText = text(1,10,string, 'FontSize', 16, 'BackgroundColor', 'white');
 [x,y] = ginput;
 
+delete(hText)
 % fit an ellipse to the inputted points
-[ellipseFitParams] = constrainedEllipseFit(x,y, p.Results.ellipseTransparentLB, p.Results.ellipseTransparentUB, []);
+[ellipseFitParams] = constrainedEllipseFit(x,y, ellipseTransparentLB, ellipseTransparentUB, []);
 
 % convert the ellipse params from transparent params to explicit params
 explicitEllipseFitParams = ellipse_transparent2ex(ellipseFitParams);
@@ -76,11 +112,53 @@ explicitEllipseFitParams = ellipse_transparent2ex(ellipseFitParams);
 pFitImplicit = ellipse_ex2im(explicitEllipseFitParams);
 % write the implicit function on teh basis of these params
 fh=@(x,y) pFitImplicit(1).*x.^2 +pFitImplicit(2).*x.*y +pFitImplicit(3).*y.^2 +pFitImplicit(4).*x +pFitImplicit(5).*y +pFitImplicit(6);
-% superimpose the ellipse using fimplicit 
+% superimpose the ellipse using fimplicit
 hold on
-fimplicit(fh,[1, videoSizeX, 1, videoSizeY],'Color', 'green','LineWidth',1);
+fHandle = fimplicit(fh,[1, videoSizeX, 1, videoSizeY],'Color', 'green','LineWidth',1);
 set(gca,'position',[0 0 1 1],'units','normalized')
 axis off;
+
+% ask if we need to redo ellipse placement
+pupilCheckChoice = GetWithDefault('>> Satisfied with ellipse fit? Enter ''y'' to proceed, or ''n'' to redraw. [y/n]', 'y');
+if ~strcmp(pupilCheckChoice, 'y')
+    delete(fHandle);
+    
+    pupilEllipseDoneFlag = false;
+    while ~pupilEllipseDoneFlag
+        string = 'Define the pupil boundary by clicking along the pupil perimeter. Press ENTER when finished.';
+        hText = text(1,10,string, 'FontSize', 16, 'BackgroundColor', 'white');
+        [x,y] = ginput;
+        
+        delete(hText)
+        % fit an ellipse to the inputted points
+        [ellipseFitParams] = constrainedEllipseFit(x,y, ellipseTransparentLB, ellipseTransparentUB, []);
+        
+        % convert the ellipse params from transparent params to explicit params
+        explicitEllipseFitParams = ellipse_transparent2ex(ellipseFitParams);
+        
+        % convert the explicit ellipse params to implicit
+        pFitImplicit = ellipse_ex2im(explicitEllipseFitParams);
+        % write the implicit function on teh basis of these params
+        fh=@(x,y) pFitImplicit(1).*x.^2 +pFitImplicit(2).*x.*y +pFitImplicit(3).*y.^2 +pFitImplicit(4).*x +pFitImplicit(5).*y +pFitImplicit(6);
+        % superimpose the ellipse using fimplicit
+        hold on
+        fHandle = fimplicit(fh,[1, videoSizeX, 1, videoSizeY],'Color', 'green','LineWidth',1);
+        set(gca,'position',[0 0 1 1],'units','normalized')
+        axis off;
+        
+        pupilCheckChoice = GetWithDefault('>> Satisfied with ellipse fit? Enter ''y'' to proceed, or ''n'' to redraw. [y/n]', 'y');
+      
+        switch pupilCheckChoice
+            case 'y'
+                pupilEllipseDoneFlag = true;
+                
+            case 'n'
+                pupilEllipseDoneFlag = false;
+                delete(fHandle);
+            otherwise
+        end
+    end
+end
 
 %% Figure out the pupil frame
 
@@ -94,10 +172,10 @@ end
 % we'll take the center of the ellipse, and then extend out along from the
 % expanded pupil radius to define the pupilFrameMask
 initialParams.pupilFrameMask = [round(explicitEllipseFitParams(2)-explicitEllipseFitParams(4)*p.Results.pupilMaskDilationFactor) ...
-                                round(videoSizeX - (explicitEllipseFitParams(1)+explicitEllipseFitParams(3)*p.Results.pupilMaskDilationFactor)) ...
-                                round(videoSizeY - (explicitEllipseFitParams(2)+explicitEllipseFitParams(4)*p.Results.pupilMaskDilationFactor)) ...
-                                round(explicitEllipseFitParams(1)-explicitEllipseFitParams(3)*p.Results.pupilMaskDilationFactor)];
-                            
+    round(videoSizeX - (explicitEllipseFitParams(1)+explicitEllipseFitParams(3)*p.Results.pupilMaskDilationFactor)) ...
+    round(videoSizeY - (explicitEllipseFitParams(2)+explicitEllipseFitParams(4)*p.Results.pupilMaskDilationFactor)) ...
+    round(explicitEllipseFitParams(1)-explicitEllipseFitParams(3)*p.Results.pupilMaskDilationFactor)];
+
 %% Figure out initial pupil range
 % make the lower bound a little smaller, and upper bound bigger, than the
 % user-inputted pupil ellipse
@@ -105,7 +183,32 @@ initialParams.pupilRange = [round(circleRadius*p.Results.pupilRangeContractor) r
 
 %% Figure out the glinFrametMask
 % Ask the user to define the position of the two glints
+fprintf('Define glint position in figure.\n')
+string = sprintf('Define the glint position by clicking each of the %d glints.', numberOfGlints);
+hText = text(1,10,string, 'FontSize', 16, 'BackgroundColor', 'white');
 [x,y] = ginput(2);
+gHandle = plot(x ,y, '+', 'Color', 'red');
+
+% ask if we need to redo glint placement
+glintCheckChoice = GetWithDefault('>> Satisfied with glint locations? Enter ''y'' to proceed, or ''n'' to redraw. [y/n]', 'y');
+if ~strcmp(glintCheckChoice, 'y')
+    delete(gHandle);   
+    glintDoneFlag = false;
+    while ~glintDoneFlag
+        string = sprintf('Define the glint position by clicking each of the %d glints.', numberOfGlints);
+                hText = text(1,10,string, 'FontSize', 16, 'BackgroundColor', 'white');
+                [x,y] = ginput(2);
+                gHandle = plot(x ,y, '+', 'Color', 'red');
+                glintCheckChoice = GetWithDefault('>> Satisfied with glint locations? Enter ''y'' to proceed, or ''n'' to redraw. [y/n]', 'y');
+        switch glintCheckChoice
+            case 'y'
+                glintDoneFlag = true;
+            case 'n'
+                glintDoneFlag = false;
+                delete(gHandle);
+        end
+    end
+end
 
 % assume the middle x coordinate of the glintFrameMask should be the
 % average of the two x positions of each glint
@@ -120,6 +223,7 @@ initialParams.glintFrameMask = [round(glintYPositionUpper - p.Results.glintMaskP
     round(videoSizeX - (glintXPosition + p.Results.glintMaskPaddingFactor)) ...
     round(videoSizeY - (glintYPositionLower + p.Results.glintMaskPaddingFactor)) ...
     round(glintXPosition - p.Results.glintMaskPaddingFactor)];
+close all
 
 %% Figure out the pupilCircleThresh
 % define a pupil mask. we're interested in figuring out what values make up
@@ -173,16 +277,16 @@ maskedIrisNaN(maskedIris == 0) = NaN;
 % proper value that will be used there, so we'll set up the environment so
 % that it'll be the same
 % gamma correct the image
-thisFrameMasked = imadjust(thisFrame,[],[],p.Results.pupilGammaCorrection);
+thisFrameMasked = imadjust(thisFrame,[],[],pupilGammaCorrection);
 
 % conver to grayscale
 %thisFrameMasked = rgb2gray(thisFrameMasked);
 
 % make the image, according to pupilFrameMask
-thisFrameMasked((1:initialParams.pupilFrameMask(1)),:) = p.Results.frameMaskValue; %top
-thisFrameMasked(:, (end - initialParams.pupilFrameMask(2):end)) = p.Results.frameMaskValue; %left
-thisFrameMasked((end - initialParams.pupilFrameMask(3):end),:) = p.Results.frameMaskValue; %bottom
-thisFrameMasked(:, (1:initialParams.pupilFrameMask(4))) = p.Results.frameMaskValue; %right
+thisFrameMasked((1:initialParams.pupilFrameMask(1)),:) = frameMaskValue; %top
+thisFrameMasked(:, (end - initialParams.pupilFrameMask(2):end)) = frameMaskValue; %left
+thisFrameMasked((end - initialParams.pupilFrameMask(3):end),:) = frameMaskValue; %bottom
+thisFrameMasked(:, (1:initialParams.pupilFrameMask(4))) = frameMaskValue; %right
 
 % smooth the image
 I = thisFrameMasked;
@@ -210,17 +314,19 @@ end
 % i don't know of an analytical way to solve this quantile function, so
 % i'll brute-force check what pupilCircleThresh value provides a cutoff
 % closest to our intended intensityDivider
-counter = 1; 
+counter = 1;
 for xx = p.Results.potentialThreshValues
-    y(counter) = quantile(double(pI(:)),xx); 
-    counter = counter+1; 
+    y(counter) = quantile(double(pI(:)),xx);
+    counter = counter+1;
 end
 [minValue, potentialIndices] = min(abs(y-intensityDivider));
 
 pupilCircleThresh = p.Results.potentialThreshValues(min(potentialIndices)-1);
 initialParams.pupilCircleThresh = pupilCircleThresh;
 
-
+if p.Results.verbose
+    initialParams
+end
 
 
 end
