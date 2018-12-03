775dd768e69adf36d591e00c344cf4d586322028
harrison-mcadams <harrison.mcadams@gmail.com>
2018-12-03 17:04:08 -0500

    clean up

--- code/makeSparklines.m
+++ code/makeSparklines.m
@@ -29,8 +29,10 @@ for ss = 2:size(sessionTable, 1)
         numberOfGoodSessions = numberOfGoodSessions + 1 ;
     end
     
-    [averageResponseStruct, trialStruct] = makeSubjectAverageResponses_interpolateLast(subjectID, 'sessions', sessionID);
-    averageResponseStructAccumulator{end + 1} = averageResponseStruct;
+    if ~isempty(sessionID)
+        [averageResponseStruct, trialStruct] = makeSubjectAverageResponses_interpolateLast(subjectID, 'sessions', sessionID);
+        averageResponseStructAccumulator{end + 1} = averageResponseStruct;
+    end
     
     
 end
@@ -52,9 +54,9 @@ colors.Melanopsin{2} = [66/255, 179/255, 213/255];
 colors.Melanopsin{3} = [26/255, 35/255, 126/255];
 
 grayColormap = colormap(gray);
-colors.LMS{1} = grayColorMap(50,:);
-colors.LMS{2} = grayColorMap(25,:);
-colors.LMS{3} = grayColorMap(1,:);
+colors.LMS{1} = grayColormap(50,:);
+colors.LMS{2} = grayColormap(25,:);
+colors.LMS{3} = grayColormap(1,:);
 
 colors.LightFlux{1} = [254/255, 235/255, 101/255];
 colors.LightFlux{2} = [228/255, 82/255, 27/255];
@@ -62,15 +64,18 @@ colors.LightFlux{3} = [77/255, 52/255, 47/255];
 
 plotFig = figure;
 hold on
-for ss = 1:length(averageResponseStructAccumulator)
+%for ss = 1:length(averageResponseStructAccumulator)
+counter = 1;
+for ss = [5,3,4,6,7,8,10, 12, 19, 1, 2, 9, 11, 13:18]
     for stimulus = 1:length(stimuli)
         
         x1 = (lastIndexToPlot - firstIndexToPlot)*(stimulus - 1) + xoffset*(stimulus - 1);
         x = x1:x1+(lastIndexToPlot - firstIndexToPlot);
         
-        response100 = averageResponseStructAccumulator{ss}.(stimuli{stimulus}).Contrast100(firstIndexToPlot:lastIndexToPlot) - yoffset*(ss-1);
-        response200 = averageResponseStructAccumulator{ss}.(stimuli{stimulus}).Contrast200(firstIndexToPlot:lastIndexToPlot) - yoffset*(ss-1);
-        response400 = averageResponseStructAccumulator{ss}.(stimuli{stimulus}).Contrast400(firstIndexToPlot:lastIndexToPlot) - yoffset*(ss-1);
+        response100 = averageResponseStructAccumulator{ss}.(stimuli{stimulus}).Contrast100(firstIndexToPlot:lastIndexToPlot) - yoffset*(counter-1);
+        response200 = averageResponseStructAccumulator{ss}.(stimuli{stimulus}).Contrast200(firstIndexToPlot:lastIndexToPlot) - yoffset*(counter-1);
+        response400 = averageResponseStructAccumulator{ss}.(stimuli{stimulus}).Contrast400(firstIndexToPlot:lastIndexToPlot) - yoffset*(counter-1);
+        
         
         plot(x, response100, 'Color', colors.(stimuli{stimulus}){1}, 'LineWidth', 2);
         plot(x, response200, 'Color', colors.(stimuli{stimulus}){2}, 'LineWidth', 2);
@@ -78,10 +83,13 @@ for ss = 1:length(averageResponseStructAccumulator)
         
         
     end
+            counter = counter + 1;
+
 end
 
-
-
+axis off
+set(gcf, 'Renderer', 'painters')
+print(plotFig, fullfile('~/Desktop','sparkline'), '-dpdf')
 
 
 
