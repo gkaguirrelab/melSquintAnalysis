00058bd077b232eb535fbd44d675b80791d6b10a
harrison-mcadams <harrison.mcadams@gmail.com>
2018-12-03 17:05:34 -0500

    new function

---
+++ code/checkPupilTrackingStatus.m
@@ -0,0 +1,4 @@
+function checkPupilTrackingStatus
+
+dataPathBase = getpref('melSquintAnalysis','melaDataPath');
+dataPathBase = fullfile(dataPathBase, 'Experiments/OLApproach_Squint/SquintToPulse/DataFiles');
