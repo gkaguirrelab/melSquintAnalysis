9c51f352bb11c69868c91f0aaec0a58e4dcf9fc9
harrison-mcadams <harrison.mcadams@gmail.com>
2018-12-04 16:47:15 -0500

    new subjects

--- code/toRun.m
+++ code/toRun.m
@@ -1,5 +1,6 @@
 trackSubject('MELA_0166', '2018-11-30_session_1', 'Protocol', 'Screening', 'resume', true);
-trackSubject('MELA_0167', '2018-11-30_session_1', 'Protocol', 'Screening', 'resume', true)
+trackSubject('MELA_0167', '2018-11-30_session_1', 'Protocol', 'Screening', 'resume', true);
+trackSubject('MELA_0169', '2018-12-03_session_1', 'Protocol', 'Screening', 'resume', true);
 trackSubject('MELA_0160', '2018-11-09_session_4', 'resume', true);
 trackSubject('MELA_0158', '2018-11-13_session_1', 'resume', true);
 trackSubject('MELA_0150', '2018-11-12_session_4', 'resume', true); 
@@ -10,3 +11,5 @@ trackSubject('MELA_0162', '2018-11-15_session_2', 'resume', true);
 trackSubject('MELA_0162', '2018-11-15_session_1', 'resume', true);
 trackSubject('MELA_0158', '2018-11-13_session_2', 'resume', true);
 trackSubject('MELA_0155', '2018-11-28_session_4', 'resume', true);
+trackSubject('MELA_0167', '2018-12-04_session_1', 'resume', true);
+trackSubject('MELA_0167', '2018-12-04_session_2', 'resume', true);
