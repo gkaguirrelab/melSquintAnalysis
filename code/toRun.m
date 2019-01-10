7e0b5499ee83f99ab328b145bc56c9cf07905e81
harrison-mcadams <harrison.mcadams@gmail.com>
2019-01-10 16:53:01 -0500

    more sessions

--- code/toRun.m
+++ code/toRun.m
@@ -1,4 +1,5 @@
-
+trackSubject('MELA_0168', '2018-12-11_session_1', 'Protocol', 'Screening', 'resume', true)
+trackSubject('MELA_0178', '2019-01-10_session_1', 'Protocol', 'Screening', 'resume', true)
 trackSubject('MELA_0174', '2018-12-17_session_1', 'Protocol', 'Screening', 'resume', true)
 trackSubject('MELA_0175', '2018-12-14_session_1', 'Protocol', 'Screening', 'resume', true)
 trackSubject('MELA_0173', '2018-12-13_session_1', 'Protocol', 'Screening', 'resume', true)
@@ -8,6 +9,8 @@ trackSubject('MELA_0166', '2018-11-30_session_1', 'Protocol', 'Screening', 'resu
 trackSubject('MELA_0167', '2018-11-30_session_1', 'Protocol', 'Screening', 'resume', true);
 trackSubject('MELA_0169', '2018-12-03_session_1', 'Protocol', 'Screening', 'resume', true);
 trackSubject('MELA_0174', '2018-12-17_session_1', 'Protocol', 'Screening', 'resume', true)
+trackSubject('MELA_0174', '2019-01-09_session_2', 'resume', true)
+trackSubject('MELA_0174', '2019-01-09_session_1', 'resume', true)
 trackSubject('MELA_0160', '2018-11-09_session_4', 'resume', true);
 trackSubject('MELA_0158', '2018-11-13_session_1', 'resume', true);
 trackSubject('MELA_0150', '2018-11-12_session_4', 'resume', true); 
