1a692550b1420032ce8f2d117029b1c5da1984c5
harrison-mcadams <harrison.mcadams@gmail.com>
2019-04-03 15:36:01 -0400

    for a given input vector, identify runs of consecutive values

---
+++ code/identifyRuns.m
@@ -0,0 +1,22 @@
+function runsCellArray = identifyRuns(inputVector)
+%{
+% here's a test vector with 3 clear runs: 1-3, 5-6, and 9-10.
+testVector = [1 2 3 5 6 9 10]; 
+runCellArray = identifyRuns(testVector);
+
+%}
+differenceVector = diff(inputVector);
+runsCellArray = [];
+runsCellArray{1} = inputVector(1);
+
+runsCounter = 1;
+for ii = 1:length(differenceVector)
+    if differenceVector(ii) == 1
+        runsCellArray{runsCounter} = [runsCellArray{runsCounter}, inputVector(ii+1)];
+    else
+        runsCounter = runsCounter + 1;
+        runsCellArray{runsCounter} = inputVector(ii+1);
+    end
+end
+
+end
