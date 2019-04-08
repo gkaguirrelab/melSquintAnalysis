e9b6284156a13a2ac3dacfaf7b054c56e30596ec
harrison-mcadams <harrison.mcadams@gmail.com>
2019-04-08 15:41:29 -0400

    list of individual videos to be processed

---
+++ code/newlySpotchecked.m
@@ -0,0 +1,8 @@
+spotCheckVideo('MELA_0177', '2019-03-20_session_3', 6, 9, 'skipParamsAdjustment', true, 'processVideo', true);
+spotCheckVideo('MELA_0177', '2019-03-20_session_3', 6, 8, 'skipParamsAdjustment', true, 'processVideo', true);
+spotCheckVideo('MELA_0177', '2019-03-20_session_3', 2, 8, 'skipParamsAdjustment', true, 'processVideo', true);
+spotCheckVideo('MELA_0177', '2019-03-20_session_3', 2, 9, 'skipParamsAdjustment', true, 'processVideo', true);
+spotCheckVideo('MELA_0177', '2019-03-20_session_3', 3, 9, 'skipParamsAdjustment', true, 'processVideo', true);
+spotCheckVideo('MELA_0177', '2019-03-06_session_2', 2, 10, 'skipParamsAdjustment', true, 'processVideo', true);
+spotCheckVideo('MELA_0177', '2019-03-06_session_2', 4, 5, 'skipParamsAdjustment', true, 'processVideo', true);
+
