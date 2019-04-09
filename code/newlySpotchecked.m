7bfad269d4379366c188c952d2e2c4207bba75fa
harrison-mcadams <harrison.mcadams@gmail.com>
2019-04-09 11:23:30 -0400

    couple of videos to be processed on mudpuppy

--- code/newlySpotchecked.m
+++ code/newlySpotchecked.m
@@ -1,8 +1,14 @@
-spotCheckVideo('MELA_0177', '2019-03-20_session_3', 6, 9, 'skipParamsAdjustment', true, 'processVideo', true);
-spotCheckVideo('MELA_0177', '2019-03-20_session_3', 6, 8, 'skipParamsAdjustment', true, 'processVideo', true);
-spotCheckVideo('MELA_0177', '2019-03-20_session_3', 2, 8, 'skipParamsAdjustment', true, 'processVideo', true);
-spotCheckVideo('MELA_0177', '2019-03-20_session_3', 2, 9, 'skipParamsAdjustment', true, 'processVideo', true);
-spotCheckVideo('MELA_0177', '2019-03-20_session_3', 3, 9, 'skipParamsAdjustment', true, 'processVideo', true);
-spotCheckVideo('MELA_0177', '2019-03-06_session_2', 2, 10, 'skipParamsAdjustment', true, 'processVideo', true);
-spotCheckVideo('MELA_0177', '2019-03-06_session_2', 4, 5, 'skipParamsAdjustment', true, 'processVideo', true);
+% started 4/8 on mudpuppy
+% spotCheckVideo('MELA_0177', '2019-03-20_session_3', 6, 9, 'skipParamsAdjustment', true, 'processVideo', true);
+% spotCheckVideo('MELA_0177', '2019-03-20_session_3', 6, 8, 'skipParamsAdjustment', true, 'processVideo', true);
+% spotCheckVideo('MELA_0177', '2019-03-20_session_3', 2, 8, 'skipParamsAdjustment', true, 'processVideo', true);
+% spotCheckVideo('MELA_0177', '2019-03-20_session_3', 2, 9, 'skipParamsAdjustment', true, 'processVideo', true);
+% spotCheckVideo('MELA_0177', '2019-03-20_session_3', 3, 9, 'skipParamsAdjustment', true, 'processVideo', true);
+% spotCheckVideo('MELA_0177', '2019-03-06_session_2', 2, 10, 'skipParamsAdjustment', true, 'processVideo', true);
+% spotCheckVideo('MELA_0177', '2019-03-06_session_2', 4, 5, 'skipParamsAdjustment', true, 'processVideo', true);
 
+% started 4/9 on mudpuppy. key was to adjust candidate theta range to force
+% to only look over the bad region. this improvment fixed 'error' frames
+% resulting from inability to pupil cut
+spotCheckVideo('MELA_0181', '2019-01-23_session_1', 1, 3, 'skipParamsAdjustment', true, 'processVideo', true);
+spotCheckVideo('MELA_0181', '2019-01-23_session_1', 1, 4, 'skipParamsAdjustment', true, 'processVideo', true);
