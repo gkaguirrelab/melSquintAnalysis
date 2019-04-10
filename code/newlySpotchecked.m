220e395f18cb314f599aaad2dc58213f4e815a0c
harrison-mcadams <harrison.mcadams@gmail.com>
2019-04-10 17:56:18 -0400

    new videos

--- code/newlySpotchecked.m
+++ code/newlySpotchecked.m
@@ -10,9 +10,27 @@
 % started 4/9 on mudpuppy. key was to adjust candidate theta range to force
 % to only look over the bad region. this improvment fixed 'error' frames
 % resulting from inability to pupil cut
-spotCheckVideo('MELA_0181', '2019-01-23_session_1', 1, 3, 'skipParamsAdjustment', true, 'processVideo', true);
-spotCheckVideo('MELA_0181', '2019-01-23_session_1', 1, 4, 'skipParamsAdjustment', true, 'processVideo', true);
+% spotCheckVideo('MELA_0181', '2019-01-23_session_1', 1, 3, 'skipParamsAdjustment', true, 'processVideo', true);
+% spotCheckVideo('MELA_0181', '2019-01-23_session_1', 1, 4, 'skipParamsAdjustment', true, 'processVideo', true);
 spotCheckVideo('MELA_0181', '2019-01-23_session_2', 1, 6, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
 spotCheckVideo('MELA_0181', '2019-01-23_session_2', 1, 9, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
 spotCheckVideo('MELA_0181', '2019-01-23_session_2', 1, 10, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
 spotCheckVideo('MELA_0175', '2019-01-29_session_1', 6, 4, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-29_session_1', 2, 10, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-29_session_1', 3, 4, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-29_session_2', 6, 2, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-31_session_3', 1, 5, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-31_session_3', 1, 9, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-31_session_3', 5, 5, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-31_session_3', 5, 10, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-31_session_3', 6, 10, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-31_session_4', 1, 9, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-31_session_4', 2, 3, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-31_session_4', 2, 5, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-31_session_4', 2, 6, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-31_session_4', 3, 2, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-31_session_4', 3, 4, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-31_session_4', 3, 5, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-31_session_4', 3, 6, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-31_session_4', 3, 7, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
+spotCheckVideo('MELA_0175', '2019-01-31_session_4', 3, 8, 'skipParamsAdjustment', true, 'processVideo', true, 'openVideo', false);
