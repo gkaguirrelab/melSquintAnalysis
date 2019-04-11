ca7b36ffa716aa658b6c6db0232481e30cdf24e4
harrison-mcadams <harrison.mcadams@gmail.com>
2019-04-11 10:55:46 -0400

    updated skip subjects

--- batch_makeAverageResponse.m
+++ batch_makeAverageResponse.m
@@ -1,7 +1,7 @@
 subjectListDirs = dir(fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'Experiments', 'OLApproach_Squint', 'SquintToPulse', 'DataFiles', 'MELA*'));
 
 subjectIDs = [];
-badSubjects = 'MELA_0127';
+badSubjects = {'MELA_0127', 'MELA_0168'};
 
 for ss = 1:length(subjectListDirs)
     
@@ -11,9 +11,36 @@ end
 
 subjectIDs = setdiff(subjectIDs, badSubjects);
 
-for ss = 1:length(subjectIDs)
+for ss = 30:length(subjectIDs)
    subjectID = subjectIDs{ss};
    
    [ averageResponseStruct.(subjectID), trialStruct ] = makeSubjectAverageResponses(subjectID, 'debugNumberOfNaNValuesPerTrial', true, 'blinkBufferFrames', [0 0], 'trialNaNThreshold', 2);
     
+end
+
+%% Do some summary plotting
+contrasts = {'100', '200', '400'};
+stimuli = {'Melanopsin', 'LMS', 'LightFlux'};
+
+for cc = 1:length(contrasts)
+    for stimulus = 1:length(stimuli)
+        averageResponseMatrix.(stimuli{stimulus}).(['Contrast', contrasts{cc}]) = [];
+    end
+end
+
+for cc = 1:length(contrasts)
+    for stimulus = 1:length(stimuli)
+        for ss = 1:length(subjectIDs)
+            clear trialStruct
+               subjectID = subjectIDs{ss};
+
+            load(fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'Experiments', 'OLApproach_Squint', 'SquintToPulse', 'DataFiles', subjectID, 'trialStruct.mat'));
+            for tt = 1:length(trialStruct.(stimuli{stimulus}).(['Contrast',num2str(contrasts{cc})])(1,:))
+                averageResponse(tt) = nanmean(trialStruct.(stimuli{stimulus}).(['Contrast',num2str(contrasts{cc})])(:,tt));
+            end
+
+            
+            averageResponseMatrix.(stimuli{stimulus}).(['Contrast', contrasts{cc}])(ss,:) = averageResponse;
+        end
+    end
 end
