ff6210248cd1a1667a553f3016c1c0d45e347a45
harrison-mcadams <harrison.mcadams@gmail.com>
2019-04-18 12:08:03 -0400

    commented out already-ran sessions

--- code/newlyProcessed.m
+++ code/newlyProcessed.m
@@ -45,3 +45,5 @@
 % trackSubject('MELA_0192', '2019-04-05_session_1', 'resume', true)
 % trackSubject('MELA_0191', '2019-04-09_session_4', 'resume', true)
 % trackSubject('MELA_0191', '2019-04-09_session_3', 'resume', true)
+% trackSubject('MELA_0191', '2019-04-11_session_2', 'resume', true)
+% trackSubject('MELA_0191', '2019-04-11_session_1', 'resume', true)
