62cf374f9c99664d5e71337206474692b70cd86d
harrison-mcadams <harrison.mcadams@gmail.com>
2019-05-15 16:50:04 -0400

    seems to be up and running

--- makeSceneGeometry.m
+++ makeSceneGeometry.m
@@ -6,6 +6,8 @@ p.addParameter('approach', 'Squint' ,@isstr);
 p.addParameter('protocol', 'SquintToPulse' ,@isstr);
 p.addParameter('resume', false, @islogical);
 p.addParameter('skipProcessing', false, @islogical);
+p.addParameter('useGUI', true, @islogical);
+
 
 p.parse(varargin{:})
 
@@ -16,23 +18,69 @@ pathParams.subject = subjectID;
 pathParams.protocol = 'SquintToPulse';
 pathParams.session = sessionID;
 
-%cameraDepthMean = load(fullfile(pathParams.dataBasePath, 'Experiments/OLApproach_Squint', pathParams.protocol, 'DataFiles', pathParams.subject, pathParams.session, 'pupilCalibration', 'distance.mat'));
-%cameraDepthMean = cameraDepthMean.distanceFromCornealApexToIRLens;
-cameraDepthMean = 24;
-cameraDepthSD = 1.4; % just a value on the order of what depthFromIrisDiameter would provide
 
 
-% Set up scene parameter bounds
-sceneParams.LB = [-15; 1; -5; cameraDepthMean-2*cameraDepthSD; .75; 0.9];
-sceneParams.LBp = [-12; 1.5; -4; cameraDepthMean-1*cameraDepthSD; .85; 0.95];
-sceneParams.UBp = [-8; 2; -3; cameraDepthMean+1*cameraDepthSD; 1.15; 1.05 ];
-sceneParams.UB = [-5; 2.5; -2; cameraDepthMean+2*cameraDepthSD; 1.25; 1.10];
+
+
 
 [runNamesList, subfoldersList] = getTrialList(pathParams, varargin{:});
 grayVideoName = fullfile(pathParams.dataSourceDirFull, pathParams.subject, pathParams.session, subfoldersList{end}, runNamesList{end});
-
+processedGrayVideoName = fullfile(pathParams.dataOutputDirBase, pathParams.subject, pathParams.session, subfoldersList{end}, strrep(runNamesList{end}, '.mp4', '_fitStage6.avi'));
 sceneGeometryFileName = fullfile(pathParams.dataOutputDirBase, pathParams.subject, pathParams.session, subfoldersList{end}, 'sceneGeometry.mat');
-pupilFileName = strrep(grayVideoName, '.mp4', '_pupil.mat');
+pupilFileName = fullfile(pathParams.dataOutputDirBase, pathParams.subject, pathParams.session, subfoldersList{end}, strrep(runNamesList{end}, '.mp4', '_pupil.mat'));
+%% load default scene geometry
 
-estimateSceneParams(pupilFileName, sceneGeometryFileName)
+if ~exist(fullfile(pathParams.dataOutputDirBase, 'defaultSceneGeometry.mat'))
+sceneGeometry = createSceneGeometry('intrinsicCameraMatrix', cameraParams.intrinsicCameraMatrix, ...
+        'radialDistortionVector', cameraParams.radialDistortionVector, ...
+        'sensorResolution', cameraParams.sensorResolution);
+save(fullfile(pathParams.dataOutputDirBase, 'defaultSceneGeometry.mat'), 'sceneGeometry', '-v7.3');
+else
+   defaultSceneGeometryFileName =  fullfile(pathParams.dataOutputDirBase, 'defaultSceneGeometry.mat');
+end
+
+%% create ellipse array list
+[recordedErrorFlag, consoleOutput] = system(['open ''' processedGrayVideoName '''']);
+
+centerFrames = GetWithDefault('Enter frames in which the eye is fixated straight ahead', []);
+centerPosition = [0; 0];
+
+upFrames = GetWithDefault('Enter frames in which the eye is looking up', []);
+upPosition = [0; 27.5/2];
+
+downFrames = GetWithDefault('Enter frames in which the eye is looking down', []);
+downPosition = [0; -27.5/2];
+
+leftFrames = GetWithDefault('Enter frames in which the eye is looking left', []);
+leftPosition = [-27.5/2; 0];
+
+rightFrames = GetWithDefault('Enter frames in which the eye is looking right', []);
+rightPosition = [27.5/2; 0];
+
+ellipseArrayList = [centerFrames, upFrames, downFrames, leftFrames, rightFrames];
+
+fixationTargetArray = [repmat(centerPosition, 1, length(centerFrames)), repmat(upPosition, 1, length(upFrames)), repmat(downPosition, 1, length(downFrames)), repmat(leftPosition, 1, length(leftFrames)), repmat(rightPosition, 1, length(rightFrames))];
+%% create scene param boundaries
+% Set up scene parameter bounds
+cameraDepthMean = load(fullfile(pathParams.dataBasePath, 'Experiments/OLApproach_Squint', 'SquintToPulse', 'DataFiles', pathParams.subject, pathParams.session, 'pupilCalibration', 'distance.mat'));
+cameraDepthSD = 1.4; % just a value on the order of what depthFromIrisDiameter would provide
+
+sceneParams.LB = [-30; -15; -15; cameraDepthMean.distanceFromCornealApexToIRLens-2*cameraDepthSD; .75; 0.9];
+sceneParams.UB = [30; 15; 15; cameraDepthMean.distanceFromCornealApexToIRLens+2*cameraDepthSD; 1.25; 1.10];
+
+sceneParams.LBp = [-15; -10; -10; cameraDepthMean.distanceFromCornealApexToIRLens-1*cameraDepthSD; .85; 0.95];
+sceneParams.UBp = [15; 10; 10; cameraDepthMean.distanceFromCornealApexToIRLens+1*cameraDepthSD; 1.15; 1.05 ];
+
+
+
+estimateSceneParams(pupilFileName, sceneGeometryFileName, ...
+    'ellipseArrayList', ellipseArrayList, ...
+    'fixationTargetArray', fixationTargetArray', ...
+    'sceneParamsLB', sceneParams.LB, ...
+    'sceneParamsUB', sceneParams.UB, ...
+    'sceneParamsLBp', sceneParams.LBp, ...
+    'sceneParamsUBp', sceneParams.UBp, ...
+    'intrinsicCameraMatrix', cameraParams.intrinsicCameraMatrix, ...
+    'radialDistortionVector', cameraParams.radialDistortionVector, ...
+    'sensorResolution', cameraParams.sensorResolution)
 end
