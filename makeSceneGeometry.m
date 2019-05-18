801cfaf4e745acf261f59004b4f3e010928bd8c8
Harrison McAdams <harrison.mcadams@gmail.com>
2019-05-18 09:41:42 -0400

    new function to pick frames for scene estimation
    opens the video file, cycles through different eye poses, and asks the user to provide these frames

--- makeSceneGeometry.m
+++ makeSceneGeometry.m
@@ -25,43 +25,30 @@ pathParams.session = sessionID;
 
 [runNamesList, subfoldersList] = getTrialList(pathParams, varargin{:});
 grayVideoName = fullfile(pathParams.dataSourceDirFull, pathParams.subject, pathParams.session, subfoldersList{end}, runNamesList{end});
-processedGrayVideoName = fullfile(pathParams.dataOutputDirBase, pathParams.subject, pathParams.session, subfoldersList{end}, strrep(runNamesList{end}, '.mp4', '_fitStage6.avi'));
+processedVideoName = fullfile(pathParams.dataOutputDirBase, pathParams.subject, pathParams.session, subfoldersList{end}, strrep(runNamesList{end}, '.mp4', '_fitStage6.avi'));
 sceneGeometryFileName = fullfile(pathParams.dataOutputDirBase, pathParams.subject, pathParams.session, subfoldersList{end}, 'sceneGeometry.mat');
 pupilFileName = fullfile(pathParams.dataOutputDirBase, pathParams.subject, pathParams.session, subfoldersList{end}, strrep(runNamesList{end}, '.mp4', '_pupil.mat'));
 %% load default scene geometry
 
 if ~exist(fullfile(pathParams.dataOutputDirBase, 'defaultSceneGeometry.mat'))
-sceneGeometry = createSceneGeometry('intrinsicCameraMatrix', cameraParams.intrinsicCameraMatrix, ...
+    sceneGeometry = createSceneGeometry('intrinsicCameraMatrix', cameraParams.intrinsicCameraMatrix, ...
         'radialDistortionVector', cameraParams.radialDistortionVector, ...
         'sensorResolution', cameraParams.sensorResolution);
-save(fullfile(pathParams.dataOutputDirBase, 'defaultSceneGeometry.mat'), 'sceneGeometry', '-v7.3');
+    save(fullfile(pathParams.dataOutputDirBase, 'defaultSceneGeometry.mat'), 'sceneGeometry', '-v7.3');
 else
-   defaultSceneGeometryFileName =  fullfile(pathParams.dataOutputDirBase, 'defaultSceneGeometry.mat');
+    defaultSceneGeometryFileName =  fullfile(pathParams.dataOutputDirBase, 'defaultSceneGeometry.mat');
 end
 
 %% create ellipse array list
-[recordedErrorFlag, consoleOutput] = system(['open ''' processedGrayVideoName '''']);
-
-centerFrames = GetWithDefault('Enter frames in which the eye is fixated straight ahead', []);
-centerPosition = [0; 0];
-
-upFrames = GetWithDefault('Enter frames in which the eye is looking up', []);
-upPosition = [0; 27.5/2];
-
-downFrames = GetWithDefault('Enter frames in which the eye is looking down', []);
-downPosition = [0; -27.5/2];
-
-leftFrames = GetWithDefault('Enter frames in which the eye is looking left', []);
-leftPosition = [27.5/2; 0];
-
-rightFrames = GetWithDefault('Enter frames in which the eye is looking right', []);
-rightPosition = [-27.5/2; 0];
-
-ellipseArrayList = [centerFrames, upFrames, downFrames, leftFrames, rightFrames];
-
-fixationTargetArray = [repmat(centerPosition, 1, length(centerFrames)), repmat(upPosition, 1, length(upFrames)), repmat(downPosition, 1, length(downFrames)), repmat(leftPosition, 1, length(leftFrames)), repmat(rightPosition, 1, length(rightFrames))];
+if p.Results.resume
+    load = true;
+else
+    load = false;
+end
+saveName = fullfile(pathParams.dataOutputDirBase, pathParams.subject, pathParams.session, subfoldersList{end}, 'ellipseArrayList.mat');
+[ellipseArrayList, fixationTargetArray] = pickFramesForSceneEstimation(processedVideoName, 'saveName', saveName, 'load', load);
 %% create scene param boundaries
-% Set up scene parameter bounds
+
 cameraDepthMean = load(fullfile(pathParams.dataBasePath, 'Experiments/OLApproach_Squint', 'SquintToPulse', 'DataFiles', pathParams.subject, pathParams.session, 'pupilCalibration', 'distance.mat'));
 cameraDepthSD = 1.4; % just a value on the order of what depthFromIrisDiameter would provide
 
@@ -72,16 +59,17 @@ sceneParams.LBp = [-15; -10; -10; cameraDepthMean.distanceFromCornealApexToIRLen
 sceneParams.UBp = [15; 10; 10; cameraDepthMean.distanceFromCornealApexToIRLens+1*cameraDepthSD; 1.15; 1.05 ];
 
 
-
-estimateSceneParams(pupilFileName, sceneGeometryFileName, ...
-    'ellipseArrayList', ellipseArrayList, ...
-    'fixationTargetArray', fixationTargetArray, ...
-    'sceneParamsLB', sceneParams.LB, ...
-    'sceneParamsUB', sceneParams.UB, ...
-    'sceneParamsLBp', sceneParams.LBp, ...
-    'sceneParamsUBp', sceneParams.UBp, ...
-    'intrinsicCameraMatrix', cameraParams.intrinsicCameraMatrix, ...
-    'radialDistortionVector', cameraParams.radialDistortionVector, ...
-    'sensorResolution', cameraParams.sensorResolution, ...
-    'verbose', true)
+if ~p.Results.skipProcessing
+    estimateSceneParams(pupilFileName, sceneGeometryFileName, ...
+        'ellipseArrayList', ellipseArrayList, ...
+        'fixationTargetArray', fixationTargetArray, ...
+        'sceneParamsLB', sceneParams.LB, ...
+        'sceneParamsUB', sceneParams.UB, ...
+        'sceneParamsLBp', sceneParams.LBp, ...
+        'sceneParamsUBp', sceneParams.UBp, ...
+        'intrinsicCameraMatrix', cameraParams.intrinsicCameraMatrix, ...
+        'radialDistortionVector', cameraParams.radialDistortionVector, ...
+        'sensorResolution', cameraParams.sensorResolution, ...
+        'verbose', true)
+end
 end
---
+++ pickFramesForSceneEstimation.m
@@ -0,0 +1,43 @@
+function [ellipseArrayList, fixationTargetArray] = pickFramesForSceneEstimation(processedVideoName, varargin)
+%% Input parser
+p = inputParser; p.KeepUnmatched = true;
+
+% Required
+p.addOptional('processedVideoName', [], @(x)(isempty(x) || ischar(x)));
+
+% Optional flow control params
+p.addParameter('directions', {'center', 'up', 'down', 'left', 'right'}, @iscellstr);
+p.addParameter('directionTargets', {[0; 0], [0; 27.5/2], [0; -27.5/2], [-27.5/2; 0], [27.5/2; 0]}, @iscell);
+p.addParameter('saveName', [], @ischar);
+p.addParameter('load', false, @islogical);
+
+% parse
+p.parse(processedVideoName, varargin{:})
+%% create ellipse array list
+if ~p.Results.load
+    [recordedErrorFlag, consoleOutput] = system(['open ''' processedVideoName '''']);
+    
+    ellipseArrayList = [];
+    fixationTargetArray = [];
+    for ii = 1:length(p.Results.directions)
+        if strcmp(p.Results.directions, 'center')
+            
+            frames.(p.Results.directions{ii}) = GetWithDefault('Enter frames in which the eye is fixated straight ahead', []);
+        else
+            frames.(p.Results.directions{ii}) = GetWithDefault(sprintf('Enter frames in which the eye is looking %s', p.Results.directions{ii}), []);
+            
+            
+        end
+        ellipseArrayList = [ellipseArrayList, frames.(p.Results.directions{ii})];
+        fixationTargetArray = [fixationTargetArray, repmat(p.Results.directionTargets{ii}, 1, length(frames.(p.Results.directions{ii})))];
+    end
+    
+    %% save out ellipse  array list and fixation target array
+    if ~isempty(p.Results.saveName)
+        save(p.Results.saveName, 'ellipseArrayList', 'fixationTargetArray', '-v7.3');
+    end
+else
+    load(p.Results.saveName);
+end
+
+end
