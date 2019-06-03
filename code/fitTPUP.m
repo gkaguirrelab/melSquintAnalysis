562270a05569216fdc17dc5e399227ea5f15bc80
harrison-mcadams <harrison.mcadams@gmail.com>
2019-06-03 10:59:13 -0400

    reorganized slightly

--- code/fitTPUP.m
+++ code/fitTPUP.m
@@ -1,8 +1,41 @@
 load('/Users/harrisonmcadams/Dropbox (Aguirre-Brainard Lab)/MELA_processing/Experiments/OLApproach_Squint/SquintToPulse/DataFiles/averageResponsePlots/groupAverageMatrix.mat');
 plotFig = figure;
 
+% TPUP params
+stimulusStruct = makeStimulusStruct;
+thePacket.stimulus = stimulusStruct;
+
+thePacket.kernel = [];
+thePacket.metaData = [];
+
+defaultParamsInfo.nInstances = 1;
+
+% Construct the model object
+temporalFit = tfeTPUP('verbosity','full');
+
+vlb=[-500, 150, 1, -10, -10, -10]; % these boundaries are necessary to specify until we change how the delay parameter is implemented in the forward model (negative delay currently means push curve to the right). also the range of the amplitude parameters is probably much larger than we need
+if offComponent
+    vlb=[-500, 150, 1, -10, -10, -10, -10]; % these boundaries are necessary to specify until we change how the delay parameter is implemented in the forward model (negative delay currently means push curve to the right). also the range of the amplitude parameters is probably much larger than we need
+end
+
+
+vub=[0, 900, 20, -0, -0, -0];
+
+if offComponent
+    vub=[0, 900, 20, -0, -0, -0, -0];
+end
+
+initialValues = [-200, 700, 5, 0, -1, 0];
+if offComponent
+    initialValues = [-200, 350, 5, -1, -1, -1, -1];
+end
+
+
 stimuli = {'Melanopsin', 'LMS', 'LightFlux'};
 responseIndicesToExclude = 40;
+
+
+
 for ii = 1:length(stimuli)
     subplotHandle = subplot(1,3,ii); hold on;
     offComponent = false;
@@ -12,33 +45,7 @@ for ii = 1:length(stimuli)
     thePacket.response.timebase = 0:1/60*1000:18.5*1000;
     thePacket.response.timebase = thePacket.response.timebase(responseIndicesToExclude:end-responseIndicesToExclude);
     
-    stimulusStruct = makeStimulusStruct;
-    thePacket.stimulus = stimulusStruct;
     
-    thePacket.kernel = [];
-    thePacket.metaData = [];
-    
-    defaultParamsInfo.nInstances = 1;
-    
-    % Construct the model object
-    temporalFit = tfeTPUP('verbosity','full');
-    
-    vlb=[-500, 600, 1, -10, -10, -10]; % these boundaries are necessary to specify until we change how the delay parameter is implemented in the forward model (negative delay currently means push curve to the right). also the range of the amplitude parameters is probably much larger than we need
-    if offComponent
-        vlb=[-500, 150, 1, -10, -10, -10, -10]; % these boundaries are necessary to specify until we change how the delay parameter is implemented in the forward model (negative delay currently means push curve to the right). also the range of the amplitude parameters is probably much larger than we need
-    end
-    
-    
-    vub=[0, 700, 20, -0, -0, -0];
-    
-    if offComponent
-        vub=[0, 600, 20, -0, -0, -0, -0];
-    end
-    
-    initialValues = [-200, 700, 5, 0, -1, 0];
-    if offComponent
-        initialValues = [-200, 350, 5, -1, -1, -1, -1];
-    end
     
     [paramsFit,fVal,modelResponseStruct] = ...
         temporalFit.fitResponse(thePacket, ...
