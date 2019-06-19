ff1295b8d390e8ada2714be17bd588913726de06
harrison-mcadams <harrison.mcadams@gmail.com>
2019-06-19 17:57:23 -0400

    new function with simulation approach to power analysis

---
+++ code/bootstrapRankSum.m
@@ -0,0 +1,45 @@
+sampleSize = 40;
+alpha = 0.05;
+
+
+%effectSizeScalarRange = 1.01:0.01:1.5;
+effectSizeScalarRange = 0:0.001:0.25;
+nBootstrapIterations = 1000;
+%hypothesis = 't-test';
+hypothesis = 'rankSum';
+
+powerDistribution = [];
+for effectSizeScalar = effectSizeScalarRange
+    probabilityDistribution = [];
+    
+    for ii = 1:nBootstrapIterations
+        
+        bootstrapIndicesControls = datasample(1:length(percentPersistentDistribution), sampleSize);
+        bootstrapIndicesPatients = datasample(1:length(percentPersistentDistribution), sampleSize);
+
+        percentPersistentControls = percentPersistentDistribution(bootstrapIndicesControls);
+        percentPersistentPatients = percentPersistentDistribution(bootstrapIndicesPatients)+effectSizeScalar;
+        
+        % enforce max percentPersistent of 100%
+        percentPersistentPatients(percentPersistentPatients>1) = 1;
+        
+        if strcmp(hypothesis, 'rankSum')
+            probability = ranksum(percentPersistentControls, percentPersistentPatients, 'tail', 'left');
+        elseif strcmp(hypothesis, 't-test')
+            [~, probability] = ttest2(percentPersistentControls, percentPersistentPatients, 'tail', 'left');
+        end
+
+        probabilityDistribution = [probabilityDistribution, probability];
+    end
+    falseNegativeRate = (sum(probabilityDistribution > alpha)/nBootstrapIterations);
+    power = 1 - falseNegativeRate;
+    powerDistribution = [powerDistribution, power];
+end
+
+figure;
+plot(effectSizeScalarRange, powerDistribution)
+title(['N = ', num2str(sampleSize), ', ', hypothesis]);
+ylabel('Power')
+xlabel('Effect Size (Patients = Controls + Effect Size)')
+hold on;
+line([0 0.25], [0.8, 0.8], 'Color', 'r', 'LineStyle', '-')
