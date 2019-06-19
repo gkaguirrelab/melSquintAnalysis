6987583faaef8e425c1fa828f3fe720bf0fea95e
harrison-mcadams <harrison.mcadams@gmail.com>
2019-06-19 17:57:45 -0400

    updated options for the initial 'mean' response

--- computeIncreasedPercentPersistentFromMean.m
+++ computeIncreasedPercentPersistentFromMean.m
@@ -3,42 +3,105 @@ function computeIncreasedPercentPersistentFromMean(newPercentPersistent, varargi
 %% Input parser
 p = inputParser; p.KeepUnmatched = true;
 
+p.addParameter('whichMeanResponse','fromParametesAcrossSubjects', @ischar)
 p.addParameter('methodToIncreasePercentPersistent', 'sameAUC', @ischar);
+p.addParameter('centralTendencyForParams', 'median', @ischar);
 
 p.parse(varargin{:});
 
 
 %% compute group mean model fit
+if strcmp(p.Results.whichMeanResponse, 'fromGroupAverageResponse')
 [modeledResponses] = fitTPUP('group', 'closePlots', true);
+    timebase = modeledResponse.Melanopsin.timebase;
+    groupResponse = modeledResponse.Melanopsin.values;
+
+    newParams = modeledResponses.params;
+
+
+elseif strcmp(p.Results.whichMeanResponse, 'fromParametesAcrossSubjects')
+
+    
+    % fill out the rest of the parameters
+    if strcmp(p.Results.centralTendencyForParams, 'mean')
+        newParams.paramMainMatrix(1) = -222.57431;
+        newParams.paramMainMatrix(2) = 527.7164;
+        newParams.paramMainMatrix(3) = 213.888;
+        newParams.paramMainMatrix(4) = 10.7443;
+        newParams.paramMainMatrix(5) = -0.4056;
+        newParams.paramMainMatrix(6) = -0.2283;
+        newParams.paramMainMatrix(7) = -2.7025;
+    elseif strcmp(p.Results.centralTendencyForParams, 'median')
+        newParams.paramMainMatrix(1) = -206.1377;
+        newParams.paramMainMatrix(2) = 500.4936;
+        newParams.paramMainMatrix(3) = 213.888;
+        newParams.paramMainMatrix(4) = 10.0222;
+        newParams.paramMainMatrix(5) = -0.40153;
+        newParams.paramMainMatrix(6) = -0.15851;
+        newParams.paramMainMatrix(7) = -2.4391;
+    end
+    newParams.paramNameCell{1} = 'delay';
+    newParams.paramNameCell{2} = 'gammaTau';
+    newParams.paramNameCell{3} = 'persistentGammaTau';
+    
+    newParams.paramNameCell{4} = 'exponentialTau';
+    newParams.paramNameCell{5} = 'amplitudeTransient';
+    newParams.paramNameCell{6} = 'amplitudeSustained';
+    newParams.paramNameCell{7} = 'amplitudePersistent';
+    
+    
+    
+    % make stimulus struct
+    stimulusStruct = makeStimulusStruct;
+    
+    % compute new modeled response with increased percent persistent
+    % instantiate the TPUP object
+    temporalFit = tfeTPUP('verbosity','full');
+    [ newModelResponseStruct ] = temporalFit.computeResponse(newParams, stimulusStruct, []);
+    
+    timebase = newModelResponseStruct.timebase;
+    groupResponse = newModelResponseStruct.values;
+
+    % determine total area under the curve for the group mean model
+    % fit, which we will fix as the AUC for the model fit for the
+    % increased percent persistent
+    meanTotalAUC = (newParams.paramMainMatrix(5) +newParams.paramMainMatrix(6) + newParams.paramMainMatrix(7));
+  
+    percentPersistent =  newParams.paramMainMatrix(7)/( newParams.paramMainMatrix(5) +  newParams.paramMainMatrix(6) +  newParams.paramMainMatrix(7));
+
+
+end
 plotFig = figure; hold on
-plot(modeledResponses.Melanopsin.timebase, modeledResponses.Melanopsin.values);
+plot(timebase, groupResponse);
 xlabel('Time (ms)');
 ylabel('Pupil area (% change from baseline');
 
-percentPersistent = modeledResponses.Melanopsin.params.paramMainMatrix(7)/(modeledResponses.Melanopsin.params.paramMainMatrix(7) + modeledResponses.Melanopsin.params.paramMainMatrix(5) + modeledResponses.Melanopsin.params.paramMainMatrix(6));
+% determine total area under the curve for the group mean model
+% fit, which we will fix as the AUC for the model fit for the
+% increased percent persistent
+meanTotalAUC = (newParams.paramMainMatrix(5) + newParams.paramMainMatrix(6) + newParams.paramMainMatrix(7));
+
+percentPersistent = newParams.paramMainMatrix(7)/(newParams.paramMainMatrix(7) + newParams.paramMainMatrix(5) + newParams.paramMainMatrix(6));
+
 minimumDetectableIncreasedPercentPersistent = newPercentPersistent;
 
 %% Compute the increased percent persistent
 if strcmp(p.Results.methodToIncreasePercentPersistent, 'sameAUC')
-    % determine total area under the curve for the group mean model
-    % fit, which we will fix as the AUC for the model fit for the
-    % increased percent persistent
-    meanTotalAUC = (modeledResponses.Melanopsin.params.paramMainMatrix(5) + modeledResponses.Melanopsin.params.paramMainMatrix(6) + modeledResponses.Melanopsin.params.paramMainMatrix(7));
-    % based on AUC, determine the amplitude of the persistent component
+   % based on AUC, determine the amplitude of the persistent component
     % for the increased percentPersistent response
     newPersistentAmplitude = minimumDetectableIncreasedPercentPersistent * meanTotalAUC;
     
     % figure out the rest of the amplitudes
-    transientToTransientPlusSustained = modeledResponses.Melanopsin.params.paramMainMatrix(5)/(modeledResponses.Melanopsin.params.paramMainMatrix(5) + modeledResponses.Melanopsin.params.paramMainMatrix(6));
+    transientToTransientPlusSustained =  newParams.paramMainMatrix(5)/( newParams.paramMainMatrix(5) +  newParams.paramMainMatrix(6));
     newParams.paramMainMatrix(7) = newPersistentAmplitude;
     newParams.paramMainMatrix(5) = (meanTotalAUC - newPersistentAmplitude) * transientToTransientPlusSustained;
     newParams.paramMainMatrix(6) = (meanTotalAUC - newPersistentAmplitude) * (1-transientToTransientPlusSustained);
     
     % fill out the rest of the parameters
-    newParams.paramMainMatrix(1) = modeledResponses.Melanopsin.params.paramMainMatrix(1);
-    newParams.paramMainMatrix(2) = modeledResponses.Melanopsin.params.paramMainMatrix(2);
-    newParams.paramMainMatrix(4) = modeledResponses.Melanopsin.params.paramMainMatrix(4);
-    newParams.paramMainMatrix(3) = modeledResponses.Melanopsin.params.paramMainMatrix(3);
+    newParams.paramMainMatrix(1) = newParams.paramMainMatrix(1);
+    newParams.paramMainMatrix(2) = newParams.paramMainMatrix(2);
+    newParams.paramMainMatrix(4) = newParams.paramMainMatrix(4);
+    newParams.paramMainMatrix(3) = newParams.paramMainMatrix(3);
     
     newParams.paramNameCell{1} = 'delay';
     newParams.paramNameCell{2} = 'gammaTau';
@@ -73,19 +136,19 @@ elseif strcmp(p.Results.methodToIncreasePercentPersistent, 'increasePersistentCo
     % for this method, only increase the amplitude of the persistent
     % component, leaving the amplitudes of the transient and sustained
     % unchanged to make our new model fit
-    newPersistentAmplitude = (minimumDetectableIncreasedPercentPersistent*(modeledResponses.Melanopsin.params.paramMainMatrix(5) + modeledResponses.Melanopsin.params.paramMainMatrix(6)))/(1 - minimumDetectableIncreasedPercentPersistent);
+    newPersistentAmplitude = (minimumDetectableIncreasedPercentPersistent*(newParams.paramMainMatrix(5) + newParams.paramMainMatrix(6)))/(1 - minimumDetectableIncreasedPercentPersistent);
     
     % fill out the rest of the params, everything else unchanged
     % figure out the rest of the amplitudes
     newParams.paramMainMatrix(7) = newPersistentAmplitude;
-    newParams.paramMainMatrix(5) = modeledResponses.Melanopsin.params.paramMainMatrix(5);
-    newParams.paramMainMatrix(6) = modeledResponses.Melanopsin.params.paramMainMatrix(6);
+    newParams.paramMainMatrix(5) = newParams.paramMainMatrix(5);
+    newParams.paramMainMatrix(6) = newParams.paramMainMatrix(6);
     
     % fill out the rest of the parameters
-    newParams.paramMainMatrix(1) = modeledResponses.Melanopsin.params.paramMainMatrix(1);
-    newParams.paramMainMatrix(2) = modeledResponses.Melanopsin.params.paramMainMatrix(2);
-    newParams.paramMainMatrix(3) = modeledResponses.Melanopsin.params.paramMainMatrix(3);
-    newParams.paramMainMatrix(4) = modeledResponses.Melanopsin.params.paramMainMatrix(4);
+    newParams.paramMainMatrix(1) = newParams.paramMainMatrix(1);
+    newParams.paramMainMatrix(2) = newParams.paramMainMatrix(2);
+    newParams.paramMainMatrix(3) = newParams.paramMainMatrix(3);
+    newParams.paramMainMatrix(4) = newParams.paramMainMatrix(4);
     
     newParams.paramNameCell{1} = 'delay';
     newParams.paramNameCell{2} = 'gammaTau';
