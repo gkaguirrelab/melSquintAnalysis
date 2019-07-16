1edf28ca52aa938c2dd77205ad8048e4006c71d1
harrison-mcadams <harrison.mcadams@gmail.com>
2019-07-16 17:10:34 -0400

    compatibile with runStages

--- code/pupilPipelineWrapper.m
+++ code/pupilPipelineWrapper.m
@@ -1,4 +1,4 @@
-function pupilPipelineWrapper(pathParams, sceneParams, cameraParams, varargin)
+function pupilPipelineWrapper(pathParams, varargin)
 
 %% Input parser
 p = inputParser; p.KeepUnmatched = true;
@@ -33,106 +33,15 @@ if ~(exist('firstRunIndex', 'var'))
     return
 end
 
-%% assemble the fit params cell array
-% most runs will be processed according by the same fitParams, so prepare a
-% cell array that lists these fitParams for each run
-if exist(fullfile(pathParams.dataOutputDirBase, pathParams.subject, pathParams.session, 'fitParams.mat'), 'file')
-    fitParams_new = load(fullfile(pathParams.dataOutputDirBase, pathParams.subject, pathParams.session, 'fitParams.mat'));
-    for rr = 1:length(pathParams.runNames)
-        fitParamsCellArray{rr} = fitParams_new;
-    end
-end
-
-if strcmp(pathParams.protocol, 'SquintToPulse')
-    for aa = 1:7
-        if exist(fullfile(pathParams.dataOutputDirBase, pathParams.subject, pathParams.session, subfolders{(aa-1)*10+1}, ['fitParams.mat']), 'file')
-            fitParams_new = load(fullfile(pathParams.dataOutputDirBase, pathParams.subject, pathParams.session, subfolders{(aa-1)*10+1}, ['fitParams.mat']));
-            if aa ~= 7
-                relevantIndices = [((aa-1)*10+1):(aa*10)];
-            else
-                relevantIndices = [61];
-            end
-            for rr = relevantIndices
-                
-                
-                
-                fitParamsCellArray{rr} = fitParams_new;
-            end
-        end
-    end
-end
-
-if strcmp(pathParams.protocol, 'Screening')
-    for tt = 1:12
-        fitParamsCellArray{tt} = load(fullfile(pathParams.dataOutputDirBase, pathParams.subject, pathParams.session, subfolders{tt}, 'fitParams.mat'));
-    end
-end
-
-% however, some runs might benefit from different params. for these runs,
-% swap the relevant fitParams in place of the default fitParams
-for rr = 1:length(pathParams.runNames)
-    if exist(fullfile(pathParams.dataOutputDirBase, pathParams.subject, pathParams.session, subfolders{rr}, ['fitParams_', pathParams.runNames{rr}(1:end-4), '.mat']), 'file')
-        fitParams_new = load(fullfile(pathParams.dataOutputDirBase, pathParams.subject, pathParams.session, subfolders{rr}, ['fitParams_', pathParams.runNames{rr}(1:end-4), '.mat']));
-        fitParamsCellArray{rr} = fitParams_new;
-    end
-end
-
-fitParams = [];
-[ defaultFitParams, ~, ~ ] = getDefaultParams(varargin{:});
 %% Run the video pipeline
 for rr = firstRunIndex:length(pathParams.runNames)
-    fitParams = fitParamsCellArray{rr}.fitParams;
-    fprintf('Analyzing subject %s, session %s, acquisition %s, %s\n', pathParams.subject, pathParams.session, subfolders{rr}(end-1:end), pathParams.runNames{rr}(1:end-4));
     
-    pathParams.grayVideoName = fullfile(pathParams.dataSourceDirFull, pathParams.subject, pathParams.session, subfolders{rr}, pathParams.runNames{rr});
+    acquisitionNumber = ceil(rr/10);
+    trialNumber = rr - (acquisitionNumber-1)*10;
     
+    stagesToRun = setdiff(1:11, [1 7 8 9 10 11]);
+    stagesToWriteToVideo = [6];
     
-    pathParams.dataOutputDirFull = fullfile(pathParams.dataOutputDirBase, pathParams.subject, pathParams.session, subfolders{rr});
-    runName = strsplit(pathParams.runNames{rr}, '.');
-    pathParams.runName = runName{1};
-    
-    if ~isfield(fitParams, 'expandPupilRange')
-        fitParams.expandPupilRange = defaultFitParams.expandPupilRange;
-    end
-    if ~isfield(fitParams, 'candidateThetas')
-        fitParams.candidateThetas = defaultFitParams.candidateThetas;
-    end
-    if ~isfield(fitParams, 'smallObjThresh')
-        fitParams.smallObjThresh = defaultFitParams.smallObjThresh;
-    end
-    if ~isfield(fitParams, 'extendBlinkWindow')
-        fitParams.extendBlinkWindow = defaultFitParams.extendBlinkWindow;
-    end
-    if ~isfield(fitParams, 'pickLargestCircle')
-        fitParams.pickLargestCircle = defaultFitParams.pickLargestCircle;
-    end
-    if ~isfield(fitParams, 'threshold')
-        fitParams.threshold = defaultFitParams.threshold;
-    end
-    
-    
-    runVideoPipeline(pathParams,...
-        'skipStageByNumber', [1 7 8 9 10 11],...
-        'useParallel', pathParams.useParallel,...
-        'verbose', pathParams.verbose, ...
-        'glintFrameMask',fitParams.glintFrameMask,'glintGammaCorrection', fitParams.glintGammaCorrection, 'numberOfGlints', fitParams.numberOfGlints, ...
-        'pupilRange', fitParams.pupilRange,'pupilFrameMask', fitParams.pupilFrameMask,'pupilCircleThresh', fitParams.pupilCircleThresh,'pupilGammaCorrection', fitParams.pupilGammaCorrection,'maskBox', fitParams.maskBox,...
-        'cutErrorThreshold', fitParams.cutErrorThreshold, 'badFrameErrorThreshold', fitParams.badFrameErrorThreshold,'glintPatchRadius', fitParams.glintPatchRadius, 'ellipseTransparentUB',fitParams.ellipseTransparentUB, ...
-        'ellipseTransparentLB',fitParams.ellipseTransparentLB, 'sceneParamsLB',sceneParams.LB, 'sceneParamsUB',sceneParams.UB, ...
-        'sceneParamsLBp',sceneParams.LBp,'sceneParamsUBp',sceneParams.UBp,...
-        'intrinsicCameraMatrix', cameraParams.intrinsicCameraMatrix, ...
-        'sensorResolution', cameraParams.sensorResolution, ...
-        'radialDistortionVector',cameraParams.radialDistortionVector, ...
-        'constraintTolerance', fitParams.constraintTolerance, ...
-        'eyeLaterality',pathParams.eyeLaterality, ...
-        'makeFitVideoByNumber',[6], ...
-        'overwriteControlFile', fitParams.overwriteControlFile, ...
-        'minRadiusProportion', fitParams.minRadiusProportion, ...
-        'expandPupilRange', fitParams.expandPupilRange, ...
-        'candidateThetas', fitParams.candidateThetas, ...
-        'smallObjThresh', fitParams.smallObjThresh, ...
-        'pickLargestCircle', fitParams.pickLargestCircle, ...
-        'extendBlinkWindow', fitParams.extendBlinkWindow, ...
-        'glintsMainDirection', 'both', 'threshold', fitParams.threshold, 'removeIsolatedGlints', true, 'glintFileName', fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_glint.mat']));
+    runStages(pathParams.subject, pathParams.session, acquisitionNumber, trialNumber, stagesToRun, stagesToWriteToVideo);
     
 end
--- code/trackSubject.m
+++ code/trackSubject.m
@@ -10,7 +10,7 @@ p.addParameter('skipProcessing', false, @islogical);
 
 p.parse(varargin{:})
 
-[ defaultFitParams, cameraParams, pathParams ] = getDefaultParams('approach', 'Squint','protocol', 'SquintToPulse');
+[ defaultFitParams, ~, pathParams, ~ ] = getDefaultParams('approach', 'Squint','protocol', 'SquintToPulse');
 
 if isnumeric(sessionID)
     sessionDir = dir(fullfile(pathParams.dataSourceDirFull, subjectID, ['2*session_', num2str(sessionID)]));
@@ -21,17 +21,7 @@ pathParams.subject = subjectID;
 pathParams.protocol = p.Results.protocol;
 pathParams.session = sessionID;
 
-%cameraDepthMean = load(fullfile(pathParams.dataBasePath, 'Experiments/OLApproach_Squint', pathParams.protocol, 'DataFiles', pathParams.subject, pathParams.session, 'pupilCalibration', 'distance.mat'));
-%cameraDepthMean = cameraDepthMean.distanceFromCornealApexToIRLens;
-cameraDepthMean = 24;
-cameraDepthSD = 1.4; % just a value on the order of what depthFromIrisDiameter would provide
 
-
-% Set up scene parameter bounds
-sceneParams.LB = [-15; 1; -5; cameraDepthMean-2*cameraDepthSD; .75; 0.9];
-sceneParams.LBp = [-12; 1.5; -4; cameraDepthMean-1*cameraDepthSD; .85; 0.95];
-sceneParams.UBp = [-8; 2; -3; cameraDepthMean+1*cameraDepthSD; 1.15; 1.05 ];
-sceneParams.UB = [-5; 2.5; -2; cameraDepthMean+2*cameraDepthSD; 1.25; 1.10];
 
 % figure out if we're resuming a session, which dictates whether we're
 % figuring out the initialParameters
@@ -70,7 +60,7 @@ end
 
 if ~p.Results.skipProcessing
     % do the tracking
-    pupilPipelineWrapper(pathParams, sceneParams, cameraParams, varargin{:});
+    pupilPipelineWrapper(pathParams, varargin{:});
 end
 
 close all
