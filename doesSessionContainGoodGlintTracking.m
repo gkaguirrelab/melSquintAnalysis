6db5738584b1b94f44c20a745d2c0ee0c0f40273
harrison-mcadams <harrison.mcadams@gmail.com>
2019-08-01 13:53:16 -0400

    can also look at saved fitParams for individual trials

--- doesSessionContainGoodGlintTracking.m
+++ doesSessionContainGoodGlintTracking.m
@@ -1,4 +1,12 @@
-function [ status] = doesSessionContainGoodGlintTracking(subjectID, sessionID)
+function [ status] = doesSessionContainGoodGlintTracking(subjectID, sessionID, varargin)
+
+p = inputParser; p.KeepUnmatched = true;
+
+p.addParameter('pickTrial',[],@isnumeric);
+
+% Parse and check the parameters
+p.parse(varargin{:});
+
 
 if isnumeric(sessionID)
     [ sessionID ] = getSessionID(subjectID, sessionID);
@@ -6,7 +14,30 @@ end
 
 [ ~, ~, pathParams ] = getDefaultParams('Protocol', 'SquintToPulse');
 
-load(fullfile(pathParams.dataOutputDirBase, subjectID, sessionID, 'pupilCalibration', 'fitParams.mat'));
+% first look for a trial specific
+
+if ~isempty(p.Results.pickTrial)
+    acquisitionNumber = p.Results.pickTrial(1);
+    trialNumber = p.Results.pickTrial(2);
+    acquisitionFolderName = sprintf('videoFiles_acquisition_%02d', acquisitionNumber);
+    
+    if ~isnumeric(trialNumber)
+        runName = trialNumber;
+    else
+        runName = sprintf('trial_%03d', trialNumber);
+    end
+    
+    
+    if exist((fullfile(pathParams.dataOutputDirBase, subjectID, sessionID, acquisitionFolderName, ['fitParams_', runName, '.mat'])))
+        load(fullfile(pathParams.dataOutputDirBase, subjectID, sessionID, acquisitionFolderName, ['fitParams_', runName, '.mat']));
+    elseif exist((fullfile(pathParams.dataOutputDirBase, subjectID, sessionID, acquisitionFolderName, ['fitParams.mat'])))
+        load(fullfile(pathParams.dataOutputDirBase, subjectID, sessionID, acquisitionFolderName, ['fitParams.mat']));
+    else
+        load(fullfile(pathParams.dataOutputDirBase, subjectID, sessionID, ['fitParams.mat']));
+    end
+else
+    load(fullfile(pathParams.dataOutputDirBase, subjectID, sessionID, 'pupilCalibration', ['fitParams.mat']));
+end
 
 if isfield(fitParams, 'threshold')
     status = true;
