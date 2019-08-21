f6dc8dc839cbe3205a57bd07246dcee365fb0ab1
harrison-mcadams <harrison.mcadams@gmail.com>
2019-08-21 13:04:02 -0400

    functions borrowed from OLApproach_Squint
    some modifications to applyValidationExclusion criteria to account for some weird subjects at the beginning of our experiment

---
+++ code/applyValidationExclusionCriteria.m
@@ -0,0 +1,213 @@
+function [ passStatus] =  applyValidationExclusionCriteria(validation, DirectionObject, varargin)
+% Looks at the results contained within a validation structure to decide if
+% these validation measurements pass the OLApproach_Squint's specific
+% exclusion criteria.
+%
+% Syntax:
+%  [ passStatus] =  applyValidationExclusionCriteria(validation, DirectionObject)
+
+% Description:
+%   This function uses the DirectionObject to determine which type of
+%   stimulus we're working with. Each stimulus type has certain criteria
+%   for both the contrast on the targeted photoreceptors, as well as the
+%   splatter on photoreceptor mechanisms nominally silenced , for what
+%   constitutes a good stimulus. These criteria are hard-coded below. The
+%   routine looks at the results contained within the validation structure,
+%   and determines if these validation measurements pass the experiment's
+%   pre-registered exclusion criteria.
+
+% Inputs:
+%	validation            - A structure containing the validation results,
+%                           most likely produced by the function
+%                           summarizeValidation
+%   DirectionObject       - The direction object for the relevant stimulus
+%                           class. This is used to determine the stimulus
+%                           type of the validation structure.
+% Optional Key-Value Pairs:
+%   verbose               - A string that can either be set to 'off' (the
+%                           default), or 'on.' If set to 'on,' then the
+%                           routine will print to the console which
+%                           validation measures, if any, do not pass our
+%                           exclusion criteria.
+% Outputs:
+%   passStatus            - A binary 0 or 1. If 1, these validation
+%                           measurements pass exclusion criteria. If 0,
+%                           these validation measurements fail exclusion
+%                           criteria.
+
+
+
+
+
+%% Parse input
+p = inputParser; p.KeepUnmatched = true;
+
+p.addParameter('verbose','off',@ischar);
+
+
+p.parse(varargin{:});
+
+%% set up the criteria
+splatterLimit = 0.2;
+minTargetedContrast = 3.5;
+
+failStatus = 0;
+
+%% apply the failure criteria
+if strcmp(DirectionObject.describe.directionParams.baseName, 'LightFlux_chrom') % light flux pulses
+    if abs(median(validation.LMSContrast)) < minTargetedContrast
+        failStatus = failStatus + 1;
+        if strcmp(p.Results.verbose, 'on')
+            
+            sprintf('LMS contrast for Light Flux stimulation too low')
+        end
+    end
+    if abs(median(validation.SMinusLMContrast)) > splatterLimit
+        failStatus = failStatus + 1;
+        if strcmp(p.Results.verbose, 'on')
+            
+            sprintf('SCone contrast for Light Flux stimulation too high')
+        end
+    end
+    if abs(median(validation.LMinusMContrast)) > splatterLimit
+        failStatus = failStatus + 1;
+        if strcmp(p.Results.verbose, 'on')
+            
+            sprintf('L-M contrast for Light Flux stimulation too high')
+        end
+    end
+    if abs(median(validation.MelanopsinContrast)) < minTargetedContrast
+        failStatus = failStatus + 1;
+        if strcmp(p.Results.verbose, 'on')
+            
+            sprintf('Melanopsin contrast for Light Flux stimulation too low')
+        end
+    end
+else
+    if isfield(DirectionObject.describe.directionParams, 'targetContrast')
+    targetedReceptors = DirectionObject.describe.directionParams.targetContrast;
+    else
+        modulationType = DirectionObject.describe.directionParams.baseName;
+       if contains(modulationType, 'LightFlux')
+           targetedReceptors = [4 4 4 4];
+       elseif contains(modulationType, 'MaxMel')
+           targetedReceptors = [0 0 0 4];
+       elseif contains(modulationType, 'MaxLMS')
+           targetedReceptors = [4 4 4 0];
+       end
+        
+    end
+    
+    if isequal(targetedReceptors, [4 4 4 0]) % LMS pulses
+        if abs(median(validation.LMSContrast)) < minTargetedContrast
+            failStatus = failStatus + 1;
+            if strcmp(p.Results.verbose, 'on')
+                
+                sprintf('LMS contrast for LMS stimulation too low')
+            end
+        end
+        if abs(median(validation.SMinusLMContrast)) > splatterLimit
+            failStatus = failStatus + 1;
+            if strcmp(p.Results.verbose, 'on')
+                
+                sprintf('SCone contrast for LMS stimulation too high')
+            end
+        end
+        if abs(median(validation.LMinusMContrast)) > splatterLimit
+            failStatus = failStatus + 1;
+            if strcmp(p.Results.verbose, 'on')
+                
+                sprintf('L-M contrast for LMS stimulation too high')
+            end
+        end
+        if abs(median(validation.MelanopsinContrast)) > splatterLimit
+            failStatus = failStatus + 1;
+            if strcmp(p.Results.verbose, 'on')
+                
+                sprintf('Melanopsin contrast for LMS stimulation too high')
+            end
+        end
+    end
+    
+    % for melanopsin stimuli, apply the exlcusion criteria
+    if isequal(targetedReceptors, [0 0 0 4]) % mel pulses
+        
+        if abs(median(validation.MelanopsinContrast)) < minTargetedContrast
+            failStatus = failStatus + 1;
+            if strcmp(p.Results.verbose, 'on')
+                
+                sprintf('Melanopsin contrast for Melanopsin stimulation too low')
+            end
+        end
+        if abs(median(validation.SMinusLMContrast)) > splatterLimit
+            failStatus = failStatus + 1;
+            if strcmp(p.Results.verbose, 'on')
+                
+                sprintf('SCone contrast for Melanopsin stimulation too high')
+            end
+            
+        end
+        if abs(median(validation.LMinusMContrast)) > splatterLimit
+            failStatus = failStatus + 1;
+            if strcmp(p.Results.verbose, 'on')
+                
+                sprintf('L-M contrast for Melanopsin stimulation too high')
+            end
+            
+        end
+        if abs(median(validation.LMSContrast)) > splatterLimit
+            failStatus = failStatus + 1;
+            if strcmp(p.Results.verbose, 'on')
+                
+                sprintf('LMS contrast for Melanopsin stimulation too high')
+            end
+            
+        end
+    end
+    
+    if isequal(targetedReceptors, [1, 2, 3, 4]) % mel/LMS combined pulses
+        
+        if abs(median(validation.MelanopsinContrast)) < minTargetedContrast
+            failStatus = failStatus + 1;
+            if strcmp(p.Results.verbose, 'on')
+                
+                sprintf('Melanopsin contrast for Mel/LMS combined stimulation too low')
+            end
+        end
+        if abs(median(validation.SMinusLMContrast)) > splatterLimit
+            failStatus = failStatus + 1;
+            if strcmp(p.Results.verbose, 'on')
+                
+                sprintf('SCone contrast for Mel/LMS combined stimulation too high')
+            end
+            
+        end
+        if abs(median(validation.LMinusMContrast)) > splatterLimit
+            failStatus = failStatus + 1;
+            if strcmp(p.Results.verbose, 'on')
+                
+                sprintf('L-M contrast for Mel/LMS combined stimulation too high')
+            end
+            
+        end
+        if abs(median(validation.LMSContrast)) < minTargetedContrast
+            failStatus = failStatus + 1;
+            if strcmp(p.Results.verbose, 'on')
+                
+                sprintf('LMS contrast for Mel/LMS combined stimulation too low')
+            end
+            
+        end
+    end
+end
+
+
+
+
+if failStatus > 0
+    passStatus = 0;
+else
+    passStatus = 1;
+end
+
+end
---
+++ code/summarizeValidation.m
@@ -0,0 +1,218 @@
+function validation = summarizeValidation(DirectionObject, varargin)
+% Summarizes validation measurements stored within the DirectionObject.
+%
+% Syntax:
+%  [ XYChromaticity, chromaticityAccumulator ] = calculateChromaticity( DirectionObject)
+
+% Description:
+%   This function loops through the validation measurements contained within a
+%   DirectionObject and packages them into a (potentially) neater
+%   structure. If plotting is enabled, the routine will also display a plot
+%   of the relevant validation measures for the user.
+
+% Inputs:
+%   DirectionObject       - The direction object for the direction in
+%                           question. Note that validation is performed on,
+%                           and saved into, these direction objects.
+% Optional Key-Value Pairs:
+%   whichValidationPrefix - A string or cell array of strings that describe
+%                           which validation measurments we're trying to
+%                           pool. Within the DirectionObject, each
+%                           validation measurement can be associated with a
+%                           label. If we only want validations with a
+%                           corresponding label to be included, we'd
+%                           specify the appropriate label or labels with
+%                           this key-value pair.
+%  plot                   - A string that controls plotting behavior. If
+%                           'on', the default, then a plot of validation
+%                           measurements will be displayed. If 'off' (or
+%                           any other string, really), then no such plot is
+%                           shown.
+
+% Outputs:
+%   validation            - A structure where each subfield is a different 
+%                           validation measurement. The contents of each
+%                           subfield is a vector, where each element is the
+%                           relevant metric from a single validation
+%                           measurement.
+
+
+
+
+%% Parse input
+p = inputParser; p.KeepUnmatched = true;
+p.addParameter('whichValidationPrefix','all',@ischar);
+p.addParameter('plot','on',@ischar);
+p.parse(varargin{:});
+
+potentialValidations = length(DirectionObject.describe.validation);
+validationIndices = [];
+for ii = 1:potentialValidations
+    if contains(DirectionObject.describe.validation(ii).label, p.Results.whichValidationPrefix) || strcmp(p.Results.whichValidationPrefix, 'all')
+        validationIndices = [validationIndices, ii];
+    end
+end
+
+% Return empty array if nothing found
+if (isempty(validationIndices))
+    validation = [];
+end
+
+ counter = 1;
+ load T_xyz1931
+ S = DirectionObject.calibration.describe.S;
+ T_xyz = SplineCmf(S_xyz1931,683*T_xyz1931,S);
+    
+for vv = validationIndices
+    
+    validation.LConeContrast(counter) = DirectionObject.describe.validation(vv).contrastActual(1,1);
+    validation.MConeContrast(counter) = DirectionObject.describe.validation(vv).contrastActual(2,1);
+    validation.SConeContrast(counter) = DirectionObject.describe.validation(vv).contrastActual(3,1);
+    validation.MelanopsinContrast(counter) = DirectionObject.describe.validation(vv).contrastActual(4,1);
+    
+    validation.LMSContrast(counter) = DirectionObject.describe.validation(vv).postreceptoralContrastActual(1,1);
+    validation.LMinusMContrast(counter) = DirectionObject.describe.validation(vv).postreceptoralContrastActual(2,1);
+    validation.SMinusLMContrast(counter) = DirectionObject.describe.validation(vv).postreceptoralContrastActual(3,1);
+    
+    %validation.backgroundLuminance(vv) = DirectionObject.describe.(potentialValidations{vv}).actualBackgroundLuminance;
+    
+    validation.backgroundLuminance(counter) = DirectionObject.describe.validation(vv).luminanceActual(1);
+    if (isfield(DirectionObject.describe.validation(vv), 'temperatures') && ~isempty(DirectionObject.describe.validation(vv).temperatures))
+        validation.boxTemperature(counter) = DirectionObject.describe.validation(vv).temperatures{1}.value(1);
+        validation.roomTemperature(counter) = DirectionObject.describe.validation(vv).temperatures{1}.value(2);
+    else
+        validation.boxTemperature(counter) = NaN;
+        validation.roomTemperature(counter) = NaN;
+    end
+    if isfield(DirectionObject.describe.validation(vv), 'stateTrackingData')
+        if (~isempty(DirectionObject.describe.validation(vv).stateTrackingData)) && numel(fieldnames(DirectionObject.describe.validation(vv).stateTrackingData)) ~= 0
+            
+            allOnSPD = DirectionObject.describe.validation(vv).stateTrackingData.powerFluctuation.spd;
+            validation.maxLuminance(counter) = T_xyz(2,:) * [allOnSPD];
+        else
+            validation.maxLuminance(counter) = NaN;
+        end
+    else
+        validation.maxLuminance(counter) = NaN;
+    end
+    counter = counter + 1;
+end
+
+if strcmp(p.Results.plot, 'on')
+    
+    % make a big plot
+    set(gcf,'un','n','pos',[.05,.05,.7,.6])
+    SConeContrastVector = cell2mat({validation.SMinusLMContrast});
+    LMSContrastVector = cell2mat({validation.LMSContrast});
+    LMinusMContrastVector = cell2mat({validation.LMinusMContrast});
+    MelanopsinContrastVector = cell2mat({validation.MelanopsinContrast});
+    
+    
+    directionName = DirectionObject.describe.directionParams.name;
+    
+    
+    title(directionName, 'Interpreter', 'none');
+    
+    
+    
+    % determine the appropriate y-axis limits
+    if contains(directionName, 'MaxLMS_')
+        intendedContrastVector = LMSContrastVector;
+        splatterVectors = [SConeContrastVector LMinusMContrastVector MelanopsinContrastVector];
+    elseif contains(directionName, 'MaxMel_')
+        intendedContrastVector = MelanopsinContrastVector;
+        splatterVectors = [SConeContrastVector LMinusMContrastVector LMSContrastVector];
+    elseif contains(directionName, 'MaxMelLMS_')
+        intendedContrastVector = [LMSContrastVector MelanopsinContrastVector];
+        splatterVectors = [SConeContrastVector LMinusMContrastVector];
+    elseif contains(directionName, 'LightFlux_')
+        intendedContrastVector = [LMSContrastVector MelanopsinContrastVector];
+        splatterVectors = [SConeContrastVector LMinusMContrastVector];
+    end
+    
+    % for the direction of interest, the y axis will be bounded
+    % between 390 and 410 unless any of the data points are outside
+    % that range. in that case, extend the range from that data
+    % point further by 5%
+    if min(intendedContrastVector*100) < 390
+        yIntendedMin = min(intendedContrastVector*100) - 5;
+        
+    else
+        yIntendedMin = 390;
+    end
+    if max(intendedContrastVector*100) > 410
+        yIntendedMax = max(intendedContrastVector*100) + 5;
+    else
+        yIntendedMax = 410;
+    end
+    
+    % for directions not of interest, the y bounds will be set
+    % between -10 and 10 again unless individual data points
+    % require that range to be extended
+    if min(splatterVectors*100) < -10
+        ySplatterMin = min(splatterVectors*100) - 5;
+        
+    else
+        ySplatterMin = -10;
+    end
+    if max(splatterVectors*100) > 10
+        ySplatterMax = max(splatterVectors*100) + 5;
+    else
+        ySplatterMax = 10;
+    end
+    
+    hold on;
+    
+    % add lines to show the boundaries of our exclusion criteria
+    line([0.5 4.5], [350 350], 'Color', 'r', 'LineStyle', '--');
+    line([0.5 4.5], [20 20], 'Color', 'r', 'LineStyle', '--');
+    line([0.5 4.5], [-20 -20], 'Color', 'r', 'LineStyle', '--');
+    
+    ylim([ySplatterMin yIntendedMax]);
+    
+    
+    
+    % putting the data together to work with the plotSpread
+    % function
+    data = horzcat({100*SConeContrastVector', 100*LMinusMContrastVector', 100*LMSContrastVector', 100*MelanopsinContrastVector'});
+    
+    % determine how many stimulus labels we are working with
+    counter = 1;
+    for ii = validationIndices
+        labelsArray{counter} = DirectionObject.describe.validation(ii).label;
+        counter = counter + 1;
+    end
+    uniqueLabels = unique(labelsArray);
+    
+    catIdxInstance = [];
+    for ii = 1:length(validationIndices)
+        for ll = 1:length(uniqueLabels)
+            if strcmp(DirectionObject.describe.validation(ii).label, uniqueLabels{ll})
+                catIdxInstance(ii) = ll-1;
+            end
+        end
+    end
+    
+    markers = {'+', 'o', '.', '*', 'o', '+', '.', '*', 'o', '+', '.', '*'};
+    catIdx = horzcat(catIdxInstance, catIdxInstance, catIdxInstance, catIdxInstance)';
+    [test] = plotSpread(data, 'categoryIdx', catIdx,  'categoryMarkers', {markers{1:length(uniqueLabels)}}, 'categoryLabels', uniqueLabels, 'xNames', {'S Cone', 'L-M', 'LMS', 'Melanopsin'});
+    
+    textString = [];
+    for ll = 1:length(uniqueLabels)
+        textString = [textString, markers{ll}, ': ', uniqueLabels{ll}, '\n'];
+    end
+        
+    text(0.25, (yIntendedMax+yIntendedMin)/2, sprintf(textString))
+
+    
+    if yIntendedMin - ySplatterMax < 100
+    else
+        
+        % apply y-axis break to more cleanly show all validation
+        % measurements on 1 subplot
+        [test] = breakyaxis([ySplatterMax yIntendedMin], 0.01, 0.1);
+    end
+    
+end
+
+end
