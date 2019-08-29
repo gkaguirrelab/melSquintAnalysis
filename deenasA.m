baf116d32f1f25603489c5455aea4960d0d64043
harrison-mcadams <harrison.mcadams@gmail.com>
2019-08-29 16:58:50 -0400

    new for deenasA

--- deenasA.m
+++ deenasA.m
@@ -1,3 +1,5 @@
+applySceneGeometryPerSession('MELA_0126',2,'resume', true);
+
 applySceneGeometryPerSession('MELA_0128',2,'resume', true);
 applySceneGeometryPerSession('MELA_0160', 1, 'resume', true)
 
