1a5fbd2848b623e32e197a586264058f06321b02
harrison-mcadams <harrison.mcadams@gmail.com>
2019-08-30 16:24:19 -0400

    forceApplySceneGeometry fro 124

--- emg.m
+++ emg.m
@@ -2,6 +2,6 @@ applySceneGeometryPerSession('MELA_0122',3,'resume', true);
 applySceneGeometryPerSession('MELA_0121',1,'resume', true);
 applySceneGeometryPerSession('MELA_0209',4,'resume', true);
 
-applySceneGeometryPerSession('MELA_0124',1,'resume', true);
+applySceneGeometryPerSession('MELA_0124',1,'resume', true, 'forceApplySceneGeometry', true);
 
 applySceneGeometryPerSession('MELA_0128',4,'resume', true);
