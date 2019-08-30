752b8828f6654258265fc9634185219241d1a306
harrison-mcadams <harrison.mcadams@gmail.com>
2019-08-30 16:22:39 -0400

    forceApplySceneGeometry for 126

--- pupil.m
+++ pupil.m
@@ -3,6 +3,6 @@ applySceneGeometryPerSession('MELA_0122',2,'resume', true);
 
 applySceneGeometryPerSession('MELA_0209',3,'resume', true);
 
-applySceneGeometryPerSession('MELA_0126',4,'resume', true);
+applySceneGeometryPerSession('MELA_0126',4,'resume', true, 'forceApplySceneGeometry', true);
 
 applySceneGeometryPerSession('MELA_0128',3,'resume', true);
