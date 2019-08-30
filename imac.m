10426bb49c1988bb07f5b1a6be3b39b6afc1216d
harrison-mcadams <harrison.mcadams@gmail.com>
2019-08-30 16:20:47 -0400

    forceApplySceneGeometry for 126

--- imac.m
+++ imac.m
@@ -2,6 +2,6 @@ applySceneGeometryPerSession('MELA_0207',2,'resume', true);
 
 applySceneGeometryPerSession('MELA_0207',3,'resume', true);
 
-applySceneGeometryPerSession('MELA_0126',3,'resume', true);
+applySceneGeometryPerSession('MELA_0126',3,'resume', true, 'forceApplySceneGeometry', true);
 
 applySceneGeometryPerSession('MELA_0131', 3, 'resume', true);
