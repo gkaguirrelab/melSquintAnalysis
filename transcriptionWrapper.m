d4e4752a44bf4bc16cd32cd5e02dd90b1721ec3c
harrison-mcadams <harrison.mcadams@gmail.com>
2019-09-03 15:09:39 -0400

    everything in MELA_analysis

--- transcriptionWrapper.m
+++ transcriptionWrapper.m
@@ -1,6 +1,6 @@
 function transcriptionWrapper(subjectNumber)
 
-load(fullfile(getpref('melSquintAnalysis', 'melaProcessingPath'), 'Experiments/OLApproach_Squint/SquintToPulse/DataFiles/', 'subjectListStruct.mat'));
+load(fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'Experiments/OLApproach_Squint/SquintToPulse/DataFiles/', 'subjectListStruct.mat'));
 
 subjectIDs = fieldnames(subjectListStruct);
 
