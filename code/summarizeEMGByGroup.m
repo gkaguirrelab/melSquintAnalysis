0619ec64c50c03d8df491939a6a3be5183009e06
harrison-mcadams <harrison.mcadams@gmail.com>
2019-10-09 15:25:43 -0400

    summarize responses over time

--- code/summarizeEMGByGroup.m
+++ code/summarizeEMGByGroup.m
@@ -39,7 +39,7 @@ for ss = 1:length(subjectIDs)
     if calculateRMS
         calculateRMSforEMG(subjectIDs{ss}, 'sessions', subjectListStruct.(subjectIDs{ss}), 'makePlots', true);
         calculateRMSforEMG(subjectIDs{ss}, 'sessions', subjectListStruct.(subjectIDs{ss}), 'makePlots', true, 'normalize', true);
-
+        
     end
     close all;
     for stimulus = 1:length(stimuli)
@@ -67,6 +67,92 @@ end
 mwaSubjects = unique(mwaSubjects);
 mwoaSubjects = unique(mwoaSubjects);
 controlSubjects = unique(controlSubjects);
+%% Analyze EMG responses over time
+windowLength = 500;
+stimuli = {'Melanopsin', 'LMS', 'LightFlux'};
+contrasts = {100, 200, 400};
+for stimulus = 1:length(stimuli)
+    for contrast = 1:length(contrasts)
+        pooledSubjectMeanResponses.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]) = [];
+    end
+end
+for ss = 1:length(subjectIDs)
+    load(fullfile(fullfile(getpref('melSquintAnalysis','melaAnalysisPath'), 'melSquintAnalysis', 'EMG', 'responseOverTime', ['windowLength_', num2str(windowLength), 'MSecs'], 'trialStructs', [subjectIDs{ss}, '.mat'])));
+    for stimulus = 1:length(stimuli)
+        for contrast = 1:length(contrasts)
+            pooledSubjectMeanResponses.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})])(ss,:) = nanmean(trialStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]).combined);
+        end
+    end
+    
+end
+
+plotFig = figure;
+resampledTimebase = 0:1/5000*windowLength:1/5000*windowLength*length(pooledSubjectMeanResponses.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})])(ss,:)) - 1/5000*windowLength;
+
+nStimuli = length(stimuli);
+nContrasts = length(contrasts);
+
+% set up color palette
+colorPalette.Melanopsin{1} = [220/255, 237/255, 200/255];
+colorPalette.Melanopsin{2} = [66/255, 179/255, 213/255];
+colorPalette.Melanopsin{3} = [26/255, 35/255, 126/255];
+
+grayColorMap = colormap(gray);
+colorPalette.LMS{1} = grayColorMap(50,:);
+colorPalette.LMS{2} = grayColorMap(25,:);
+colorPalette.LMS{3} = grayColorMap(1,:);
+colorPalette.LS = colorPalette.LMS;
+
+colorPalette.LightFlux{1} = [254/255, 235/255, 101/255];
+colorPalette.LightFlux{2} = [228/255, 82/255, 27/255];
+colorPalette.LightFlux{3} = [77/255, 52/255, 47/255];
+
+for ss = 1:nStimuli
+    
+    % pick the right subplot for the right stimuli
+    ax.(['ax', num2str(ss)]) = subplot(nStimuli,1,ss);
+    title(stimuli{ss})
+    hold on
+    
+    for cc = 1:nContrasts
+        
+        % make thicker plot lines
+        lineProps.width = 1;
+        
+        % adjust color
+        lineProps.col{1} = colorPalette.(stimuli{ss}){cc};
+        
+        % plot
+        axis.(['ax', num2str(cc)]) = mseb(resampledTimebase, nanmean(pooledSubjectMeanResponses.(stimuli{ss}).(['Contrast', num2str(contrasts{cc})])), std(pooledSubjectMeanResponses.(stimuli{ss}).(['Contrast', num2str(contrasts{cc})]))/(sqrt(size(pooledSubjectMeanResponses.(stimuli{ss}).(['Contrast', num2str(contrasts{cc})]),1))), lineProps);
+        
+        legendText{cc} = ([num2str(contrasts{cc}), '% Contrast, N = ', num2str(size(pooledSubjectMeanResponses.(stimuli{ss}).(['Contrast', num2str(contrasts{cc})]), 1))]);
+        
+    end
+    
+    legend(legendText, 'Location', 'NorthEast')
+    legend('boxoff')
+    
+    % add line for pulse onset
+    line([1.5,  5.5], [-0.5, -0.5], 'Color', 'k', 'LineWidth', 5, 'HandleVisibility','off');
+    
+    % spruce up axes
+    
+    
+    xlabel('Time (s)')
+    ylabel('EMG Activity (STD)')
+    
+end
+
+linkaxes([ax.ax1, ax.ax2, ax.ax3]);
+
+fullSavePath = fullfile(getpref('melSquintAnalysis','melaAnalysisPath'), 'melSquintAnalysis', 'EMG', 'responseOverTime', ['windowLength_', num2str(windowLength), 'MSecs']);
+
+if ~exist(fullSavePath)
+    mkdir(fullSavePath);
+end
+
+print(plotFig, fullfile(fullSavePath, 'combinedGroupAverage'), '-dpdf', '-fillpage')
+
 
 %% Display results
 
