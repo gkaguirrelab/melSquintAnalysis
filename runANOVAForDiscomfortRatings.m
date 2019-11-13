8f5a4a82dcc03210bb46eef6176108ffd67d75f5
harrison-mcadams <harrison.mcadams@gmail.com>
2019-11-13 15:52:12 -0500

    adjusted for new approach to ANOVA
    based on lines of best fit for individual subjects

--- runANOVAForDiscomfortRatings.m
+++ runANOVAForDiscomfortRatings.m
@@ -1,3 +1,149 @@
+%% ANOVA on slopes and intercepts
+dataBasePath = getpref('melSquintAnalysis','melaDataPath');
+
+
+load(fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'Experiments/OLApproach_Squint/SquintToPulse/DataFiles/', 'subjectListStruct.mat'));
+
+subjectIDs = fieldnames(subjectListStruct);
+
+controlDiscomfort = [];
+mwaDiscomfort = [];
+mwoaDiscomfort = [];
+
+stimuli = {'Melanopsin', 'LMS', 'LightFlux'};
+contrasts = {100, 200, 400};
+
+
+
+for stimulus = 1:length(stimuli)
+    for contrast = 1:length(contrasts)
+        controlDiscomfort.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]) = [];
+        mwaDiscomfort.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]) = [];
+        mwoaDiscomfort.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]) = [];
+    end
+end
+
+
+for ss = 1:length(subjectIDs)
+    
+    
+    group = linkMELAIDToGroup(subjectIDs{ss});
+    
+    analysisBasePath = fullfile(getpref('melSquintAnalysis','melaAnalysisPath'), 'Experiments/OLApproach_Squint/SquintToPulse/DataFiles/', subjectIDs{ss});
+    fileName = 'audioTrialStruct_final.mat';
+    
+    for stimulus = 1:length(stimuli)
+        for contrast = 1:length(contrasts)
+            if strcmp(group, 'c')
+                load(fullfile(analysisBasePath, fileName));
+                controlDiscomfort.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})])(end+1) = nanmedian(trialStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]));
+            elseif strcmp(group, 'mwa')
+                load(fullfile(analysisBasePath, fileName));
+                mwaDiscomfort.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})])(end+1) = nanmedian(trialStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]));
+                
+            elseif strcmp(group, 'mwoa')
+                load(fullfile(analysisBasePath, fileName));
+                mwoaDiscomfort.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})])(end+1) = nanmedian(trialStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]));
+            else
+                fprintf('Subject %s has group %s\n', subjectIDs{ss}, group);
+            end
+        end
+    end
+    
+end
+
+
+slopeCellArrayHeader = {'Stimulus', 'Group', 'Slope'};
+interceptCellArrayHeader = {'Stimulus', 'Group', 'Intercept'};
+
+x = [log10(100), log10(200), log10(400)];
+groups = {'controls', 'mwa', 'mwoa'};
+plotFig = figure; hold on;
+for ss = 1:20
+    for stimulus = 1:length(stimuli)
+        for group = 1:length(groups)
+            
+            if strcmp(groups{group}, 'controls')
+                y = [controlDiscomfort.(stimuli{stimulus}).Contrast100(ss), controlDiscomfort.(stimuli{stimulus}).Contrast200(ss), controlDiscomfort.(stimuli{stimulus}).Contrast400(ss)];
+                rowAdjuster = 1;
+                color = 'k';
+            elseif strcmp(groups{group}, 'mwa')
+                y = [mwaDiscomfort.(stimuli{stimulus}).Contrast100(ss), mwaDiscomfort.(stimuli{stimulus}).Contrast200(ss), mwaDiscomfort.(stimuli{stimulus}).Contrast400(ss)];
+                rowAdjuster = 2;
+                color = 'b';
+            elseif strcmp(groups{group}, 'mwoa')
+                y = [mwoaDiscomfort.(stimuli{stimulus}).Contrast100(ss), mwoaDiscomfort.(stimuli{stimulus}).Contrast200(ss), mwoaDiscomfort.(stimuli{stimulus}).Contrast400(ss)];
+                rowAdjuster = 3;
+                color = 'r';
+            end
+            
+            
+          coeffs = polyfit(x, y, 1);
+          slopeCellArray{(ss-1)*3+rowAdjuster+(stimulus-1)*60, 1} = stimuli{stimulus};
+          slopeCellArray{(ss-1)*3+rowAdjuster+(stimulus-1)*60, 2} = groups{group};
+          slopeCellArray{(ss-1)*3+rowAdjuster+(stimulus-1)*60, 3} = coeffs(1);
+          
+          interceptCellArray{(ss-1)*3+rowAdjuster+(stimulus-1)*60, 1} = stimuli{stimulus};
+          interceptCellArray{(ss-1)*3+rowAdjuster+(stimulus-1)*60, 2} = groups{group};
+          interceptCellArray{(ss-1)*3+rowAdjuster+(stimulus-1)*60, 3} = coeffs(2);
+          
+          anovaTable((ss-1)*3+rowAdjuster+(stimulus-1)*60, 1) = stimulus;
+          anovaTable((ss-1)*3+rowAdjuster+(stimulus-1)*60, 2) = group;
+          anovaTable((ss-1)*3+rowAdjuster+(stimulus-1)*60, 3) = coeffs(1);
+          anovaTable((ss-1)*3+rowAdjuster+(stimulus-1)*60, 4) = coeffs(2);
+          subplot(1,3,stimulus); hold on;
+          fittedX = linspace(min(x), max(x), 200);
+          fittedY = polyval(coeffs, fittedX);
+          plot(fittedX, fittedY, 'LineWidth', 1, 'Color', color);
+        end
+       
+    end
+   
+   
+end
+
+for stimulus = 1:length(stimuli)
+    
+    subplot(1,3,stimulus); hold on;
+    
+    controlSlopeMean = median(anovaTable(1:3:60+(stimulus-1)*60, 3));
+    mwaSlopeMean = median(anovaTable(2:3:60+(stimulus-1)*60, 3));
+    mwoaSlopeMean = median(anovaTable(3:3:60+(stimulus-1)*60, 3));
+    
+    controlInterceptMean = median(anovaTable(1:3:60+(stimulus-1)*60, 4));
+    mwaInterceptMean = median(anovaTable(2:3:60+(stimulus-1)*60, 4));
+    mwoaInterceptMean = median(anovaTable(3:3:60+(stimulus-1)*60, 4));
+    
+    fittedX = linspace(min(x), max(x), 200);
+    fittedY = polyval([controlSlopeMean, controlInterceptMean], fittedX);
+    plot(fittedX, fittedY, 'LineWidth', 5, 'Color', 'k');
+    
+    fittedX = linspace(min(x), max(x), 200);
+    fittedY = polyval([mwaSlopeMean, mwaInterceptMean], fittedX);
+    plot(fittedX, fittedY, 'LineWidth', 5, 'Color', 'b');
+    
+    fittedX = linspace(min(x), max(x), 200);
+    fittedY = polyval([mwoaSlopeMean, mwoaInterceptMean], fittedX);
+    plot(fittedX, fittedY, 'LineWidth', 5, 'Color', 'r');
+
+    title(stimuli{stimulus})
+    xticks(x);
+    xticklabels({'100%', '200%', '400%'});
+    xlabel('Contrast')
+    ylabel('Discomfort')
+   
+
+end
+
+export_fig(plotFig, fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', 'discomfortRatings', 'discomfortFitLines.png'));
+ 
+% save out csv files
+slopeCellArray = vertcat({'Stimulus', 'Group', 'Slope'}, slopeCellArray);
+interceptCellArray = vertcat({'Stimulus', 'Group', 'Intercept'}, interceptCellArray);
+
+cell2csv(fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', 'discomfortRatings', 'slopes.csv'), slopeCellArray);
+cell2csv(fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', 'discomfortRatings', 'intercepts.csv'), interceptCellArray);
+
 %% get all discomfort ratings responses
 generateMatrix = false;
 
