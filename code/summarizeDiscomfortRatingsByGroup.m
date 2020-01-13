de9a75ab88fa3a2a8bc01e4db2e026fd6f79d1cf
harrison-mcadams <harrison.mcadams@gmail.com>
2020-01-13 12:21:05 -0500

    added new summary plot
    transparency is really wonky

--- code/summarizeDiscomfortRatingsByGroup.m
+++ code/summarizeDiscomfortRatingsByGroup.m
@@ -133,5 +133,124 @@ end
 
 export_fig(plotFig, fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', 'discomfortRatings', 'mediansWithIQR.pdf'));
 
+%% summary figrue proposed by geoff:
+stimuli = {'LightFlux', 'Melanopsin', 'LMS'};
+groups = {'control', 'mwa', 'mwoa'};
+
+%[ discomfortRatingsStruct ] = loadDiscomfortRatings;
+%[ slope, intercept, meanRating ] = fitLineToResponseModality('discomfortRatings', 'makePlots', false, 'makeCSV', false);
+
+x = [log10(100), log10(200), log10(400)];
+
+counter = 1;
+[ha, pos] = tight_subplot(3,3, 0.04);
+
+for group = 1:length(groups)
+
+    for stimulus = 1:length(stimuli)
+        
+        if strcmp(groups{group}, 'control')
+            color = 'k';
+        elseif strcmp(groups{group}, 'mwa')
+            color = 'b';
+        elseif strcmp(groups{group}, 'mwoa')
+            color = 'r';
+        end
+        
+        
+        axes(ha(counter)); hold on;        
+        result = discomfortRatingsStruct.([groups{group}, 'Discomfort']);
+        data = [result.(stimuli{stimulus}).Contrast100; result.(stimuli{stimulus}).Contrast200; result.(stimuli{stimulus}).Contrast400];
+        
+        plotSpread(data', 'xValues', x, 'xNames', {'100%', '200%', '400%'}, 'distributionColors', color)
+        set(findall(gcf,'type','line'),'markerSize',13) 
+        yticks([0 5 10])
+        
+        if stimulus == 1
+            ylabel('Discomfort Rating')
+            ylim([0 10.5]);
+            yticks([0 5 10]);
+            yticklabels([0 5 10]);
+        end
+
+        
+        counter = counter + 1;
 
 
+        
+    end
+end
+
+% add titles to the columns
+axes(ha(1));
+title(stimuli{1});
+axes(ha(2));
+title(stimuli{2});
+axes(ha(3));
+title(stimuli{3});
+
+           
+% add medians
+counter = 1;
+for group = 1:length(groups)
+    
+    for stimulus = 1:length(stimuli)
+        
+        if strcmp(groups{group}, 'control')
+            color = 'k';
+        elseif strcmp(groups{group}, 'mwa')
+            color = 'b';
+        elseif strcmp(groups{group}, 'mwoa')
+            color = 'r';
+        end
+        
+                axes(ha(counter)); hold on;        
+        result = discomfortRatingsStruct.([groups{group}, 'Discomfort']);
+        
+        plot(x, [median(result.(stimuli{stimulus}).Contrast100), median(result.(stimuli{stimulus}).Contrast200), median(result.(stimuli{stimulus}).Contrast400)], '.', 'Color', color, 'MarkerSize', 25)
+        counter = counter + 1;
+    end
+end
+
+% add line fits
+counter = 1;
+for group = 1:length(groups)
+    
+    for stimulus = 1:length(stimuli)
+        
+        if strcmp(groups{group}, 'control')
+            color = 'k';
+            groupName = 'controls';
+        elseif strcmp(groups{group}, 'mwa')
+            color = 'b';
+            groupName = 'mwa';
+        elseif strcmp(groups{group}, 'mwoa')
+            color = 'r';
+            groupName = 'mwoa';
+        end
+        y = x*mean(slope.(groupName).(stimuli{stimulus})) + mean(intercept.(groupName).(stimuli{stimulus}));
+        axes(ha(counter)); hold on;
+        plot(x,y, 'Color', color)
+        counter = counter + 1;
+        
+        
+    end
+end
+
+
+
+% to make the markers transparent. for whatever reason, the effect doesn't
+% stick so you have to run it manually?
+drawnow()
+test = findall(gcf,'type','line');
+for ii = 1:length(test)
+    if strcmp(test(ii).DisplayName, '100%') ||  strcmp(test(ii).DisplayName, '200%') ||  strcmp(test(ii).DisplayName, '400%')
+        hMarkers = [];
+        hMarkers = test(ii).MarkerHandle;
+        hMarkers.EdgeColorData(4) = 75;
+    end
+end
+set(gcf, 'Position', [600 558 760 420]);
+set(gcf, 'Renderer', 'painters');
+
+export_fig(gcf, fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', 'discomfortRatings', 'summary_groupxstimulus.pdf'))
