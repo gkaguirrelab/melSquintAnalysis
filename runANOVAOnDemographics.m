bf32b1134f2494ca843ecce739503446d9a5ad6f
harrison-mcadams <harrison.mcadams@gmail.com>
2020-01-14 15:59:46 -0500

    new ANOVA routine
    to examine differences between groups on clinical and demographic bits

---
+++ runANOVAOnDemographics.m
@@ -0,0 +1,70 @@
+function runANOVAOnDemographics(resultType)
+
+dataBasePath = getpref('melSquintAnalysis','melaDataPath');
+load(fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'Experiments/OLApproach_Squint/SquintToPulse/DataFiles/', 'subjectListStruct.mat'));
+subjectIDs = fieldnames(subjectListStruct);
+
+
+pathToSurveyData = fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'surveyMelanopsinAnalysis', 'MELA_ScoresSurveyData_Squint.xlsx');
+surveyTable = readtable(pathToSurveyData);
+columnNames = surveyTable.Properties.VariableNames;
+
+% if you're not sure what the name of the result is, display all options
+if strcmp(resultType, '')
+    fprintf('Possible result types include:\n')
+    
+    columnNames
+    
+    return
+end
+
+
+
+controlResults = [];
+mwaResults = [];
+mwoaResults = [];
+
+
+resultColumn = find(contains(columnNames, resultType));
+
+for ss = 1:length(subjectIDs)
+    subjectRow = find(contains(surveyTable{:,1}, subjectIDs{ss}));
+    result = (cell2mat(surveyTable{subjectRow,resultColumn}));
+    
+    if strcmp(result, 'Male')
+        result = 1;
+    elseif strcmp(result, 'Female')
+        result = 0;
+    else
+        result = str2num(result);
+    end
+    
+    if isempty(result)
+        result = NaN;
+    end
+    
+    group = linkMELAIDToGroup(subjectIDs{ss});
+    
+    
+    if strcmp(group, 'c')
+        controlResults(end+1) = result;
+    elseif strcmp(group, 'mwa')
+        mwaResults(end+1) = result;
+        
+    elseif strcmp(group, 'mwoa')
+        mwoaResults(end+1) = result;
+    else
+        fprintf('Subject %s has group %s\n', subjectIDs{ss}, group);
+    end
+end
+
+% concatenate together into a matrix, in which different columns represent
+% results from different groups
+table = vertcat(controlResults, mwaResults, mwoaResults);
+
+% run one-way anova
+[p,tbl,stats] = anova1(table')
+
+fprintf('<strong>Post-hoc testing results:</strong>\n')
+multcompare(stats)
+end
