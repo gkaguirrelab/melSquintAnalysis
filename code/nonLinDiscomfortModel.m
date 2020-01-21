b02d56527bb8512f0aaf2cb31c9021f1276bf107
gkaguirre <gkaguirre@me.com>
2020-01-21 18:08:32 -0500

    Improving plots, speed, and reporting of results
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/nonLinDiscomfortModel.m
+++ code/nonLinDiscomfortModel.m
@@ -1,14 +1,16 @@
 discomfortStruct = loadDiscomfortRatings();
 
-groups = {'controlDiscomfort','mwaDiscomfort','mwoaDiscomfort'};
+groups = {'control','mwa','mwoa'};
 colors = {'k','b','r'};
+params = {'melScale','mikowski','slope','intercept'};
+BinWidths = [0.025,0.1,0.1,0.25];
+nBoots = 1000;
 figure
-
 options = optimset('fmincon');
 options.Display = 'off';
 
 p =[];
-iqrs=[];
+pB = [];
 
 for ii = 1:length(groups)
     
@@ -48,21 +50,25 @@ for ii = 1:length(groups)
     Lc = reshape(Lc,1,180);
     
     % Assemble the discomfort ratings
+    groupField = [groups{ii} 'Discomfort'];
     dVeridical = [ ...
-        discomfortStruct.(groups{ii}).Melanopsin.Contrast100; ...
-        discomfortStruct.(groups{ii}).Melanopsin.Contrast200; ...
-        discomfortStruct.(groups{ii}).Melanopsin.Contrast400; ...
-        discomfortStruct.(groups{ii}).LMS.Contrast100; ...
-        discomfortStruct.(groups{ii}).LMS.Contrast200; ...
-        discomfortStruct.(groups{ii}).LMS.Contrast400; ...
-        discomfortStruct.(groups{ii}).LightFlux.Contrast100; ...
-        discomfortStruct.(groups{ii}).LightFlux.Contrast200; ...
-        discomfortStruct.(groups{ii}).LightFlux.Contrast400; ...
+        discomfortStruct.(groupField).Melanopsin.Contrast100; ...
+        discomfortStruct.(groupField).Melanopsin.Contrast200; ...
+        discomfortStruct.(groupField).Melanopsin.Contrast400; ...
+        discomfortStruct.(groupField).LMS.Contrast100; ...
+        discomfortStruct.(groupField).LMS.Contrast200; ...
+        discomfortStruct.(groupField).LMS.Contrast400; ...
+        discomfortStruct.(groupField).LightFlux.Contrast100; ...
+        discomfortStruct.(groupField).LightFlux.Contrast200; ...
+        discomfortStruct.(groupField).LightFlux.Contrast400; ...
         ];
     
+    % Anonymous functions for the model
+    myModel = @(k) ((k(1).*Mc).^k(2) + Lc.^k(2)).^(1/k(2));
+    myLogLinFit = @(k,m) m(1).*log10(myModel(k))+m(2);
+        
     %% Bootstrap
-    pB = [];
-    for bb = 1:1000
+    for bb = 1:nBoots
         
         % Resample across columns (subjects) with replacement
         d = dVeridical(:,datasample(1:20,20));
@@ -70,23 +76,44 @@ for ii = 1:length(groups)
         % Reshape d to a vector
         d = reshape(d,1,180);
         
-        % Anonymous functions for the model
-        myModel = @(k) ((k(1).*Mc).^k(2) + Lc.^k(2)).^(1/k(2));
-        myLogLinFit = @(k,m) m(1).*log10(myModel(k))+m(2);
+        % Objective function
         myObj = @(p) sqrt(sum( (d - myLogLinFit(p(1:2),p(3:4))).^2 ));
         
         % Fit that sucker
-        pB(bb,:) = fmincon(myObj,[1 1 1 1],[],[],[],[],[0.1 1 0 -10],[2 Inf Inf 10],[],options);
+        pB(ii,bb,:) = fmincon(myObj,[1 1 1 1],[],[],[],[],[0.1 1 0 -10],[2 5 Inf 10],[],options);
+       
     end
     
-    % Obtain the mean and 95% CI on the p values
-    p(ii,:) = median(pB);
-    iqrs(ii,:)=iqr(pB);
-    
+    % Obtain the p values and plot these
+    p = median(squeeze(pB(ii,:,:)));    
     subplot(1,3,ii)
-    plot(log10(myModel(p(ii,1:2))),d,['*' colors{ii}])
+    plot(log10(myModel(p(1:2))),d,['*' colors{ii}])
     hold on
-    refline(p(ii,3),p(ii,4))
+    refline(p(3),p(4))
     ylim([0 10]);
+    title(groups{ii});
     
 end
+
+% Plot a histogram of the parameter values by groups across bootstraps
+figure
+for pp=1:4
+    subplot(2,2,pp);
+    histogram(squeeze(pB(1,:,pp)),'FaceColor',colors{1},'EdgeColor','none','BinWidth',BinWidths(pp))
+    hold on
+    histogram(squeeze(pB(2,:,pp)),'FaceColor',colors{2},'EdgeColor','none','BinWidth',BinWidths(pp))
+    histogram(squeeze(pB(3,:,pp)),'FaceColor',colors{3},'EdgeColor','none','BinWidth',BinWidths(pp))
+    title(params{pp});
+
+    outline = [params{pp} ' [95 CI] --- '];
+    for ii = 1:3
+        vals = sort(squeeze(pB(ii,:,pp)));
+        p = median(vals);
+        p95low = vals(round(nBoots*0.05));
+        p95hi = vals(round(nBoots*0.95));
+        outline = sprintf([outline groups{ii} ': %2.2f [%2.2f - %2.2f]; '],p,p95low,p95hi);
+    end
+    fprintf([outline '\n']);
+    
+end
+
