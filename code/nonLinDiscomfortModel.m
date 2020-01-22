0b5648350d8134bf85bf6998304f5c8bbd6a7b47
harrison-mcadams <harrison.mcadams@gmail.com>
2020-01-22 14:27:17 -0500

    now new plot markers for each stimulus type

--- code/nonLinDiscomfortModel.m
+++ code/nonLinDiscomfortModel.m
@@ -90,21 +90,53 @@ for ii = 1:length(groups)
     
     % Obtain the median param values and plot these
     p = median(squeeze(pB(ii,:,:)));    
-    subplot(1,3,ii)
-    h = scatter(log10(myModel(p(1:2))),reshape(dVeridical,1,180),'o','MarkerFaceColor',colors{ii},'MarkerEdgeColor','none');
-    h.MarkerFaceAlpha = .2;
-    hold on
+    subplot(1,3,ii);  hold on
+    %h = scatter(log10(myModel(p(1:2))),reshape(dVeridical,1,180),'o','MarkerFaceColor',colors{ii},'MarkerEdgeColor','none');
+    
+    dVeridical_reshaped = reshape(dVeridical,1,180);
+    
+    ipRGCContrastValues = unique(log10(myModel(p(1:2))));
+    ipRGCContrastValues_Mel = [ipRGCContrastValues(1), ipRGCContrastValues(4), ipRGCContrastValues(7)];
+    ipRGCContrastValues_LMS = [ipRGCContrastValues(2), ipRGCContrastValues(5), ipRGCContrastValues(8)];
+    ipRGCContrastValues_LightFlux = [ipRGCContrastValues(3), ipRGCContrastValues(6), ipRGCContrastValues(9)];
+    
+    melResponses = dVeridical(1:3,:);
+    lmsResponses = dVeridical(4:6,:);
+    LFResponses = dVeridical(7:9,:);
+    
+    m = scatter(repmat(ipRGCContrastValues_Mel, 1, 20), melResponses(:), '^', 'MarkerFaceColor',colors{ii},'MarkerEdgeColor','none');
+    m.MarkerFaceAlpha = .2;
+    
+    lms = scatter(repmat(ipRGCContrastValues_LMS, 1, 20), lmsResponses(:), 's', 'MarkerFaceColor',colors{ii},'MarkerEdgeColor','none');
+    lms.MarkerFaceAlpha = .2;
+    
+    lf = scatter(repmat(ipRGCContrastValues_LightFlux, 1, 20), LFResponses(:), 'o', 'MarkerFaceColor',colors{ii},'MarkerEdgeColor','none');
+    lf.MarkerFaceAlpha = .2;
+    
+   
     
     % Add the median discomfort ratings across subjects
-    plot(log10(myMedianModel(p(1:2))),median(dVeridical,2),['o' colors{ii}],'MarkerSize',14)
+    % plot(log10(myMedianModel(p(1:2))),median(dVeridical,2),['o' colors{ii}],'MarkerSize',14)
+    melMedian = plot(ipRGCContrastValues_Mel, [median(discomfortRatingsStruct.(groupField).Melanopsin.Contrast100), median(discomfortRatingsStruct.(groupField).Melanopsin.Contrast200), median(discomfortRatingsStruct.(groupField).Melanopsin.Contrast400)], ['^' colors{ii}],'MarkerSize',14);
+    lmsMedian = plot(ipRGCContrastValues_LMS, [median(discomfortRatingsStruct.(groupField).LMS.Contrast100), median(discomfortRatingsStruct.(groupField).LMS.Contrast200), median(discomfortRatingsStruct.(groupField).LMS.Contrast400)], ['s' colors{ii}],'MarkerSize',16);
+    LFMedian = plot(ipRGCContrastValues_LightFlux, [median(discomfortRatingsStruct.(groupField).LightFlux.Contrast100), median(discomfortRatingsStruct.(groupField).LightFlux.Contrast200), median(discomfortRatingsStruct.(groupField).LightFlux.Contrast400)], ['o' colors{ii}],'MarkerSize',14);
+
     
     % Add the model fit line
-    refline(p(3),p(4))
+    reflineHandle = refline(p(3),p(4));
+    reflineHandle.Color = colors{ii};
     ylim([0 10]);
     xlim([1.5 3]);
     xticks([log10(50) log10(100) log10(200) log10(400) log10(800)])
     xticklabels({'0.5','1','2','4','8'})
     title(groups{ii});
+    
+    if ii == 1
+        [legendHandle, icons] = legend([melMedian, lmsMedian, LFMedian], 'Melanopsin', 'Cones', 'LightFlux', 'Location', 'NorthWest', 'box', 'off');
+        icons(5).MarkerSize = 7;
+        icons(7).MarkerSize = 7;
+        icons(9).MarkerSize = 7;
+    end
     
 end
 
