081f812847e1903aef6f681d0a600c20592e9ab7
Harrison McAdams <harrison.mcadams@gmail.com>
2020-02-13 15:09:02 -0500

    scene geometry up and running for deuteranopes

--- applySceneGeometryPerSession.m
+++ applySceneGeometryPerSession.m
@@ -3,10 +3,12 @@ function applySceneGeometryPerSession(subjectID, sessionID, varargin)
 p = inputParser; p.KeepUnmatched = true;
 
 p.addParameter('resume', true ,@islogical);
+p.addParameter('Protocol', 'SquintToPulse' ,@isstr);
+p.addParameter('experimentNumber', []);
 p.addParameter('checkStatus', false ,@islogical);
 p.addParameter('debug', false ,@islogical);
 p.addParameter('reprocessEverything', false ,@islogical);
-p.addParameter('forceApplySceneGeometryOnly', false ,@islogical);
+p.addParameter('forceApplySceneGeometryOnly', true ,@islogical);
 p.addParameter('videoRange', [] );
 p.addParameter('cutErrorThreshold', 2, @isnumeric);
 
@@ -18,15 +20,16 @@ p.parse(varargin{:})
 
 %% Convert session ID from numerical value to the entire string, if inputted as number
 if isnumeric(sessionID)
-    [ sessionID ] = getSessionID(subjectID, sessionID, 'Protocol', 'SquintToPulse');
+    [ sessionID ] = getSessionID(subjectID, sessionID, 'Protocol', p.Results.Protocol);
 end
 
 %% Get some params
 
-[~, ~, pathParams] = getDefaultParams('approach', 'Squint', 'Protocol', 'SquintToPulse');
+[~, ~, pathParams] = getDefaultParams('approach', 'Squint', 'Protocol', p.Results.Protocol);
 pathParams.subject = subjectID;
 pathParams.session = sessionID;
-[pathParams.runNames, subfolders] = getTrialList(pathParams, 'Protocol', 'SquintToPulse');
+pathParams.experimentName = p.Results.experimentNumber;
+[pathParams.runNames, subfolders] = getTrialList(pathParams, 'Protocol', p.Results.Protocol);
 
 
 %% If we're resuming, figure out which trial we're resuming from
@@ -38,7 +41,7 @@ else
     runsToBeProcessed = [];
     for rr = 1:length(pathParams.runNames) - 1
         if ~strcmp(pathParams.runNames{rr}, 'trial_001.mp4')
-            if ~exist(fullfile(pathParams.dataOutputDirBase, subjectID, sessionID, subfolders{rr}, [pathParams.runNames{rr}(1:end-4), '_finalFit.avi']), 'file')
+            if ~exist(fullfile(pathParams.dataOutputDirBase, subjectID, p.Results.experimentNumber, sessionID, subfolders{rr}, [pathParams.runNames{rr}(1:end-4), '_finalFit.avi']), 'file')
                 runsToBeProcessed = [runsToBeProcessed, rr];
                 
             end
@@ -71,7 +74,11 @@ end
 
 if p.Results.checkStatus
     
-    fprintf('For subject %s, %s, need to process:\n', subjectID, sessionID)
+    if isempty(p.Results.experimentNumber)
+        fprintf('For subject %s, %s, need to process:\n', subjectID, sessionID)
+    else
+        fprintf('For subject %s, %, %s, need to process:\n', p.Results.experimentNumber, subjectID, sessionID)
+    end
     for ii = 1:length(runsToBeProcessed)
         acquisitionNumber = ceil(runsToBeProcessed(ii)/10);
         trialNumber = runsToBeProcessed(ii) - (acquisitionNumber-1)*10;
@@ -98,8 +105,11 @@ for rr = runsToBeProcessed
         trialNumber = rr - (acquisitionNumber-1)*10;
         
         stillTrying = true; tryAttempt = 0;
-        fprintf('Processing %s, %s, acquisition %d, trial %d\n', subjectID, sessionID, acquisitionNumber, trialNumber);
-        
+        if isempty(p.Results.experimentNumber)
+            fprintf('Processing %s, %s, acquisition %d, trial %d\n', subjectID, sessionID, acquisitionNumber, trialNumber);
+        else
+            fprintf('Processing %s %s, %s, acquisition %d, trial %d\n', subjectID, p.Results.experimentNumber, sessionID, acquisitionNumber, trialNumber);
+        end
         if ~p.Results.debug
             while stillTrying
                 try
@@ -109,18 +119,18 @@ for rr = runsToBeProcessed
                             
                             stagesToRun = [2 3];
                             stagesToWriteToVideo = [];
-                            runStages(subjectID, sessionID, acquisitionNumber, trialNumber, stagesToRun, stagesToWriteToVideo, 'Protocol', 'SquintToPulse');
+                            runStages(subjectID, sessionID, acquisitionNumber, trialNumber, stagesToRun, stagesToWriteToVideo, 'Protocol', p.Results.Protcol, 'experimentNumber', p.Results.experimentNumber);
                             fitParams = getDefaultParams;
                             editFitParams(subjectID, sessionID, acquisitionNumber, 'trialNumber', trialNumber, 'paramName', 'threshold', 'paramValue', fitParams.threshold);
                         end
                         % only perform aggressive cutting if it hasn't been
                         % performed yet
-                        controlFileName = fopen(fullfile(getpref('melSquintAnalysis','melaProcessingPath'), 'Experiments/OLApproach_Squint/SquintToPulse/DataFiles/', subjectID, sessionID, sprintf('videoFiles_acquisition_%02d', acquisitionNumber), sprintf('trial_%03d_controlFile.csv', trialNumber)));
+                        controlFileName = fopen(fullfile(getpref('melSquintAnalysis','melaProcessingPath'), 'Experiments/OLApproach_Squint/', p.Results.Protocol, 'DataFiles/', subjectID, p.Results.experimentNumber, sessionID, sprintf('videoFiles_acquisition_%02d', acquisitionNumber), sprintf('trial_%03d_controlFile.csv', trialNumber)));
                         if p.Results.reprocessEverything
                             performAggressiveCutting(subjectID, sessionID, acquisitionNumber, trialNumber, 'cutErrorThreshold', p.Results.cutErrorThreshold);
                         else
                             
-                            if exist(fullfile(getpref('melSquintAnalysis','melaProcessingPath'), 'Experiments/OLApproach_Squint/SquintToPulse/DataFiles/', subjectID, sessionID, sprintf('videoFiles_acquisition_%02d', acquisitionNumber), sprintf('trial_%03d_controlFile.csv', trialNumber)))
+                            if exist(fullfile(getpref('melSquintAnalysis','melaProcessingPath'), 'Experiments/OLApproach_Squint', p.Results.Protocol, 'DataFiles/', subjectID, p.Results.experimentNumber, sessionID, sprintf('videoFiles_acquisition_%02d', acquisitionNumber), sprintf('trial_%03d_controlFile.csv', trialNumber)))
                                 controlFileContents = textscan(controlFileName,'%s', 'Delimiter',',');
                                 indices = strfind(controlFileContents{1}, 'cutErrorThreshold');
                                 cutErrorThresholdIndex = find(~cellfun(@isempty,indices));
@@ -137,16 +147,24 @@ for rr = runsToBeProcessed
                             end
                         end
                     end
-                    applySceneGeometry(subjectID, sessionID, acquisitionNumber, trialNumber);
+                    applySceneGeometry(subjectID, sessionID, acquisitionNumber, trialNumber, 'Protocol', p.Results.Protocol, 'experimentNumber', p.Results.experimentNumber);
                     stillTrying = false;
                 catch
                     tryAttempt = tryAttempt + 1;
                     if tryAttempt > 5
                         stillTrying = false;
-                        warning(sprintf('Failed to process %s, %s, acquisition %d, trial %d', subjectID, sessionID, acquisitionNumber, trialNumber));
-                        
+                        if isempty(p.Results.experimentNumber)
+                            warning(sprintf('Failed to process %s, %s, acquisition %d, trial %d', subjectID, sessionID, acquisitionNumber, trialNumber));
+                        else
+                            warning(sprintf('Failed to process %s, %s, %s, acquisition %d, trial %d', subjectID, p.Results.experimentNumber, sessionID, acquisitionNumber, trialNumber));
+                        end
+                            
                         spacesAdjustErrorLogPath = strrep(errorLogPath, 'Dropbox (Aguirre-Brainard Lab)', 'Dropbox\ \(Aguirre-Brainard\ Lab\)');
-                        system(['echo "', sprintf('Failed to process %s, %s, acquisition %d, trial %d', subjectID, sessionID, acquisitionNumber, trialNumber), '" >> ', [spacesAdjustErrorLogPath, errorLogFileName]]);
+                        if isempty(p.Results.experimentNumber)
+                            system(['echo "', sprintf('Failed to process %s, %s, acquisition %d, trial %d', subjectID, sessionID, acquisitionNumber, trialNumber), '" >> ', [spacesAdjustErrorLogPath, errorLogFileName]]);
+                        else
+                            system(['echo "', sprintf('Failed to process %s, %s, %s, acquisition %d, trial %d', subjectID, p.Results.experimentNumber, sessionID, acquisitionNumber, trialNumber), '" >> ', [spacesAdjustErrorLogPath, errorLogFileName]]);
+                        end
                     end
                     
                 end
@@ -158,18 +176,18 @@ for rr = runsToBeProcessed
                     
                     stagesToRun = [2 3];
                     stagesToWriteToVideo = [];
-                    runStages(subjectID, sessionID, acquisitionNumber, trialNumber, stagesToRun, stagesToWriteToVideo, 'Protocol', 'SquintToPulse');
+                    runStages(subjectID, sessionID, acquisitionNumber, trialNumber, stagesToRun, stagesToWriteToVideo, 'Protocol', p.Results.Protocol', 'experimentNumber', p.Results.experimentNumber);
                     fitParams = getDefaultParams;
-                    editFitParams(subjectID, sessionID, acquisitionNumber, 'trialNumber', trialNumber, 'paramName', 'threshold', 'paramValue', fitParams.threshold);
+                    editFitParams(subjectID, sessionID, acquisitionNumber, 'trialNumber', trialNumber, 'paramName', 'threshold', 'paramValue', fitParams.threshold, 'Protocol', p.Results.Protocol, 'experimentNumber', p.Results.experimentNumber);
                 end
                 % only perform aggressive cutting if it hasn't been
                 % performed yet
-                controlFileName = fopen(fullfile(getpref('melSquintAnalysis','melaProcessingPath'), 'Experiments/OLApproach_Squint/SquintToPulse/DataFiles/', subjectID, sessionID, sprintf('videoFiles_acquisition_%02d', acquisitionNumber), sprintf('trial_%03d_controlFile.csv', trialNumber)));
+                controlFileName = fopen(fullfile(getpref('melSquintAnalysis','melaProcessingPath'), 'Experiments/OLApproach_Squint/', p.Results.Protocol, 'DataFiles', subjectID, p.Results.experimentNumber, sessionID, sprintf('videoFiles_acquisition_%02d', acquisitionNumber), sprintf('trial_%03d_controlFile.csv', trialNumber)));
                 if p.Results.reprocessEverything
                     performAggressiveCutting(subjectID, sessionID, acquisitionNumber, trialNumber, 'cutErrorThreshold', p.Results.cutErrorThreshold);
                 else
                     
-                    if exist(fullfile(getpref('melSquintAnalysis','melaProcessingPath'), 'Experiments/OLApproach_Squint/SquintToPulse/DataFiles/', subjectID, sessionID, sprintf('videoFiles_acquisition_%02d', acquisitionNumber), sprintf('trial_%03d_controlFile.csv', trialNumber)))
+                    if exist(fullfile(getpref('melSquintAnalysis','melaProcessingPath'), 'Experiments/OLApproach_Squint/', p.Results.Protocol, '/DataFiles/', subjectID, p.Results.experimentNumber, sessionID, sprintf('videoFiles_acquisition_%02d', acquisitionNumber), sprintf('trial_%03d_controlFile.csv', trialNumber)))
                         controlFileContents = textscan(controlFileName,'%s', 'Delimiter',',');
                         indices = strfind(controlFileContents{1}, 'cutErrorThreshold');
                         cutErrorThresholdIndex = find(~cellfun(@isempty,indices));
@@ -186,7 +204,7 @@ for rr = runsToBeProcessed
                     end
                 end
             end
-            applySceneGeometry(subjectID, sessionID, acquisitionNumber, trialNumber);
+            applySceneGeometry(subjectID, sessionID, acquisitionNumber, trialNumber, 'Protocol', p.Results.Protocol, 'experimentNumber', p.Results.experimentNumber);
             stillTrying = false;
             
         end
