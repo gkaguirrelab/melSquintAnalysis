6ba022a1391b799784a8bd290398ad8b0767cd14
Harrison McAdams <harrison.mcadams@gmail.com>
2020-03-04 18:17:53 -0500

    new function to plot deuteranope results across modalities

---
+++ code/plotDeuteranopeResult.m
@@ -0,0 +1,103 @@
+function plotDeuteranopeResult(experiment1ResultsStruct, experiment2ResultsStruct, trichromatStruct, varargin)
+
+%% Input parser
+p = inputParser; p.KeepUnmatched = true;
+
+p.addParameter('savePath', [] ,@isstr);
+p.addParameter('saveName', [] ,@isstr);
+p.addParameter('yLims', [] ,@isnumeric);
+p.addParameter('yLabel', [] ,@ischar);
+
+
+p.parse(varargin{:})
+
+%% Do the plotting
+
+
+
+
+stimuli = fieldnames(experiment1ResultsStruct);
+
+plotFig = figure;
+for stimulus = 1:length(stimuli)
+    subplot(1,3,stimulus); hold on;
+    title(stimuli{stimulus});
+    
+    if ~isempty(experiment1ResultsStruct)
+    data = [experiment1ResultsStruct.(stimuli{stimulus}).Contrast100; experiment1ResultsStruct.(stimuli{stimulus}).Contrast200; experiment1ResultsStruct.(stimuli{stimulus}).Contrast400];
+    plotSpread(data', 'xValues', [log10(100), log10(200), log10(400)], 'distributionColors', 'k')
+    plot([log10(100), log10(200), log10(400)], [median(experiment1ResultsStruct.(stimuli{stimulus}).Contrast100), median(experiment1ResultsStruct.(stimuli{stimulus}).Contrast200), median(experiment1ResultsStruct.(stimuli{stimulus}).Contrast400)], '*', 'Color', 'k')
+    experiment1Plot = plot([log10(100), log10(200), log10(400)], [median(experiment1ResultsStruct.(stimuli{stimulus}).Contrast100), median(experiment1ResultsStruct.(stimuli{stimulus}).Contrast200), median(experiment1ResultsStruct.(stimuli{stimulus}).Contrast400)], 'Color', 'k');
+    end
+    
+    if ~isempty(experiment2ResultsStruct)
+        data = [experiment2ResultsStruct.(stimuli{stimulus}).Contrast400; experiment2ResultsStruct.(stimuli{stimulus}).Contrast800; experiment2ResultsStruct.(stimuli{stimulus}).Contrast1200];
+        plotSpread(data', 'xValues', [log10(400), log10(800), log10(1200)], 'distributionColors', 'r')
+        plot([log10(400), log10(800), log10(1200)], [median(experiment2ResultsStruct.(stimuli{stimulus}).Contrast400), median(experiment2ResultsStruct.(stimuli{stimulus}).Contrast800), median(experiment2ResultsStruct.(stimuli{stimulus}).Contrast1200)], '*', 'Color', 'r')
+        experiment2Plot = plot([log10(400), log10(800), log10(1200)], [median(experiment2ResultsStruct.(stimuli{stimulus}).Contrast400), median(experiment2ResultsStruct.(stimuli{stimulus}).Contrast800), median(experiment2ResultsStruct.(stimuli{stimulus}).Contrast1200)], 'Color', 'r');
+    end
+    
+    if ~isempty(trichromatStruct)
+        trichromatStimulusName = stimuli{stimulus};
+        if strcmp(trichromatStimulusName, 'LS')
+            trichromatStimulusName = 'LMS';
+        end
+        lineProps.col{1} = 'b';
+        errorLower = [[abs(median(trichromatStruct.(trichromatStimulusName).Contrast100)), abs(median(trichromatStruct.(trichromatStimulusName).Contrast200)), abs(median(trichromatStruct.(trichromatStimulusName).Contrast400))] - [abs(prctile(trichromatStruct.(trichromatStimulusName).Contrast100, 25)), abs(prctile(trichromatStruct.(trichromatStimulusName).Contrast200, 25)), abs(prctile(trichromatStruct.(trichromatStimulusName).Contrast400, 25))]];
+        errorUpper = abs([abs(prctile(trichromatStruct.(trichromatStimulusName).Contrast100, 75)), abs(prctile(trichromatStruct.(trichromatStimulusName).Contrast200, 75)), abs(prctile(trichromatStruct.(trichromatStimulusName).Contrast400, 75))] - [abs(median(trichromatStruct.(trichromatStimulusName).Contrast100)), abs(median(trichromatStruct.(trichromatStimulusName).Contrast200)), abs(median(trichromatStruct.(trichromatStimulusName).Contrast400))]);
+        
+        errorToPlot(1,1:3, 1) = errorUpper;
+        errorToPlot(1,1:3, 2) = errorLower;
+        trichromatPlot = plot([log10(100), log10(200), log10(400)], [abs(median(trichromatStruct.(trichromatStimulusName).Contrast100)), abs(median(trichromatStruct.(trichromatStimulusName).Contrast200)), abs(median(trichromatStruct.(trichromatStimulusName).Contrast400))], 'Color', 'b');
+        mseb([log10(100), log10(200), log10(400)], [abs(median(trichromatStruct.(trichromatStimulusName).Contrast100)), abs(median(trichromatStruct.(trichromatStimulusName).Contrast200)), abs(median(trichromatStruct.(trichromatStimulusName).Contrast400))], ...
+            errorToPlot, lineProps, 1);
+        
+        
+    end
+    
+    if isempty(experiment2ResultsStruct)
+        xticks([log10(100), log10(200), log10(400)]);
+        xticklabels({'100%', '200%', '400%'});
+        
+    elseif isempty(experiment1ResultsStruct) && isempty(trichromatResultsStruct)
+        xticks([log10(400), log10(800), log10(1200)]);
+        xticklabels({'400%', '800%', '1200%'});
+        
+    else
+        xticks([log10(100), log10(200), log10(400), log10(800), log10(1200)]);
+        xticklabels({'100%', '200%', '400%', '800%', '1200%'});
+    end
+    xtickangle(45);
+    xlabel('Contrast')
+    
+    ylim(p.Results.yLims);
+    ylabel(p.Results.yLabel)
+    
+    
+    
+    if stimulus == 3
+        if isempty(experiment2ResultsStruct) || isempty(experiment1ResultsStruct)
+                    if isempty(experiment2ResultsStruct) && ~isempty(experiment1ResultsStruct) && ~isempty(trichromatStruct)
+                        legend([experiment1Plot, trichromatPlot], 'Experiment 1', 'Trichromats');
+                        legend('boxoff')
+                    end
+        elseif isempty(trichromatStruct)
+            legend([experiment1Plot, experiment2Plot], 'Experiment 1', 'Experiment 2');
+            legend('boxoff')
+
+        else
+            legend([experiment1Plot, experiment2Plot, trichromatPlot], 'Experiment 1', 'Experiment 2', 'Trichromats');
+            legend('boxoff')
+        end
+    end
+end
+
+
+
+
+
+set(plotFig, 'Position', [680 460 968 518]);
+export_fig(plotFig, fullfile(p.Results.savePath, p.Results.saveName));
+
+
+end
