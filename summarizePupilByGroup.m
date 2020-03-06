e9eb41b287ca99ac52185854ecf057b444f67f57
harrison-mcadams <harrison.mcadams@gmail.com>
2020-03-06 17:07:26 -0500

    added summary plot similar to discomfort ratings

--- summarizePupilByGroup.m
+++ summarizePupilByGroup.m
@@ -625,3 +625,134 @@ plotSpreadResults(percentPersistent, 'contrasts', {100,200, 400}, 'yLims', [-5 1
 plotSpreadResults(totalResponseAmplitude, 'contrasts', {100,200, 400}, 'yLims', [0 10], 'yLabel', 'Total Response Amplitude', 'saveName', fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', 'pupil/TPUP', 'totalResponseAmplitdueByGroup_combinedMigraine.pdf'))
 
 
+%% summary figrue proposed by geoff:
+stimuli = {'LightFlux', 'Melanopsin', 'LMS'};
+groups = {'controls', 'mwa', 'mwoa'};
+
+[ pupilStruct ] = loadPupilResponses;
+[ slope, intercept, meanRating ] = fitLineToResponseModality('pupil', 'makePlots', false, 'makeCSV', false, 'responseMetric', 'AUC');
+
+x = [log10(100), log10(200), log10(400)];
+
+counter = 1;
+[ha, pos] = tight_subplot(3,3, 0.04);
+
+for stimulus = 1:length(stimuli)
+    
+    for group = 1:length(groups)
+        
+        if strcmp(groups{group}, 'controls')
+            color = 'k';
+        elseif strcmp(groups{group}, 'mwa')
+            color = 'b';
+        elseif strcmp(groups{group}, 'mwoa')
+            color = 'r';
+        end
+        
+        
+        axes(ha(counter)); hold on;
+        result = pupilStruct.AUC.([groups{group}]);
+        data = [result.(stimuli{stimulus}).Contrast100; result.(stimuli{stimulus}).Contrast200; result.(stimuli{stimulus}).Contrast400];
+        
+        plotSpread(data', 'xValues', x, 'xNames', {'100%', '200%', '400%'}, 'distributionColors', color)
+        set(findall(gcf,'type','line'),'markerSize',13)
+        yticks([0 5 10])
+        
+        if group  == 1
+            if stimulus == 1
+                ylabel({'{\bf\fontsize{15} Light Flux}'; 'Amplitude of Constriction'})
+            elseif stimulus == 2
+                ylabel({'{\bf\fontsize{15} Melanopsin}'; 'Amplitude of Constriction'})
+                
+            elseif stimulus == 3
+                ylabel({'{\bf\fontsize{15} LMS}'; 'Amplitude of Constriction'})
+                
+            end
+            
+           
+            yticks([0:100:500]);
+            yticklabels([0 100 200 300 400 500]);
+        end
+         ylim([0 500]);
+        
+        counter = counter + 1;
+        
+        
+        
+    end
+end
+
+% add titles to the columns
+axes(ha(1));
+title({'\fontsize{15} Controls'});
+axes(ha(2));
+title({'\fontsize{15} MwA'});
+axes(ha(3));
+title({'\fontsize{15} MwoA'});
+
+
+% add medians
+counter = 1;
+for stimulus = 1:length(stimuli)
+    
+    for group = 1:length(groups)
+        
+        if strcmp(groups{group}, 'controls')
+            color = 'k';
+        elseif strcmp(groups{group}, 'mwa')
+            color = 'b';
+        elseif strcmp(groups{group}, 'mwoa')
+            color = 'r';
+        end
+        
+        axes(ha(counter)); hold on;
+        result = pupilStruct.AUC.([groups{group}]);
+        
+        plot(x, [median(result.(stimuli{stimulus}).Contrast100), median(result.(stimuli{stimulus}).Contrast200), median(result.(stimuli{stimulus}).Contrast400)], '.', 'Color', color, 'MarkerSize', 25)
+        counter = counter + 1;
+    end
+end
+
+% add line fits
+counter = 1;
+for stimulus = 1:length(stimuli)
+    
+    for group = 1:length(groups)
+        
+        
+        if strcmp(groups{group}, 'controls')
+            color = 'k';
+            groupName = 'controls';
+        elseif strcmp(groups{group}, 'mwa')
+            color = 'b';
+            groupName = 'mwa';
+        elseif strcmp(groups{group}, 'mwoa')
+            color = 'r';
+            groupName = 'mwoa';
+        end
+        y = x*mean(slope.(groupName).(stimuli{stimulus})) + mean(intercept.(groupName).(stimuli{stimulus}));
+        axes(ha(counter)); hold on;
+        plot(x,y, 'Color', color)
+        counter = counter + 1;
+        
+        
+    end
+end
+
+
+
+% to make the markers transparent. for whatever reason, the effect doesn't
+% stick so you have to run it manually?
+drawnow()
+test = findall(gcf,'type','line');
+for ii = 1:length(test)
+    if strcmp(test(ii).DisplayName, '100%') ||  strcmp(test(ii).DisplayName, '200%') ||  strcmp(test(ii).DisplayName, '400%')
+        hMarkers = [];
+        hMarkers = test(ii).MarkerHandle;
+        hMarkers.EdgeColorData(4) = 75;
+    end
+end
+set(gcf, 'Position', [600 558 1060 620]);
+set(gcf, 'Renderer', 'painters');
+
+export_fig(gcf, fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', 'pupil', 'summary_groupxstimulus.pdf'))
