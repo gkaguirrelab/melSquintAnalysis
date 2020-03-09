d372d4b8083f2c1e9eba86715769462d1da97009
harrison-mcadams <harrison.mcadams@gmail.com>
2020-03-09 14:26:22 -0400

    added more plotting

--- analyzeDroppedFrames.m
+++ analyzeDroppedFrames.m
@@ -1,8 +1,10 @@
-function analyzeDroppedFrames(varargin)
+function [droppedFramesTrialStruct, droppedFramesMeanStruct] = analyzeDroppedFrames(varargin)
 
 %% collect some inputs
 p = inputParser; p.KeepUnmatched = true;
-p.addParameter('resume',false,@islogical);
+
+p.addParameter('range','pulse',@ischar);
+p.addParameter('whichBadFrames','blinks',@ischar);
 
 % Parse and check the parameters
 p.parse(varargin{:});
@@ -13,150 +15,179 @@ load(fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'Experiments/OLA
 
 subjectIDs = fieldnames(subjectListStruct);
 
-pooledTotalDroppedFrames.Controls = [];
-pooledDroppedFramesDuringPulse.Controls = [];
-pooledDroppedFramesOutsidePulse.Controls = [];
-pooledNumberGoodTrials.Controls = [];
-pooledProportionGoodTrials.Controls = [];
-
-pooledTotalDroppedFrames.CombinedMigraineurs = [];
-pooledDroppedFramesDuringPulse.CombinedMigraineurs = [];
-pooledDroppedFramesOutsidePulse.CombinedMigraineurs = [];
-pooledNumberGoodTrials.CombinedMigraineurs = [];
-pooledProportionGoodTrials.CombinedMigraineurs = [];
-
-stimuli = {'Melanopsin', 'LightFlux', 'LMS'};
+stimuli = {'LightFlux', 'Melanopsin', 'LMS'};
 contrasts = {100, 200, 400};
+groups = {'controls', 'mwa', 'mwoa'};
+groupColors = {'k', 'b', 'r'};
 
-% determine indices corresponding to when the pulse occurs
-timebase = 0:1/60:18.5;
-[~, pulseOnsetIndex] =  min(abs(1.5-timebase));
-[~, pulseOffsetIndex] =  min(abs(5.5-timebase));
-
-for ss = 1:length(subjectIDs)
-    
-    numberGoodTrials = 0;
-    
-    resultsDir = fullfile(getpref('melSquintAnalysis','melaAnalysisPath'), 'melSquintAnalysis', 'pupil', 'trialStructs');
-    load(fullfile(resultsDir, [subjectIDs{ss}, '_trialStruct_radiusSmoothed.mat']));
-    
-    % determine number of included trials
+for group = 1:length(groups)
     for stimulus = 1:length(stimuli)
         for contrast = 1:length(contrasts)
-            numberGoodTrialsInThisStimulusCondition = size(trialStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]), 1);
-            
-            numberGoodTrials = numberGoodTrials + numberGoodTrialsInThisStimulusCondition;
-            
+            droppedFramesTrialStruct.(groups{group}).(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]) = [];
+            droppedFramesMeanStruct.(groups{group}).(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]) = [];
+
         end
     end
-    
-    % determine proportion of included trials
-    nSessions = length(subjectListStruct.(subjectIDs{ss}));
-    nTrialsPerSession = 54;
-    proportionGoodTrials = numberGoodTrials/(nSessions * nTrialsPerSession);
-    
-    group = linkMELAIDToGroup(subjectIDs{ss});
-    
-    % determine the number of dropped frames
-    proportionDroppedFramesPerTrial = [];
-    proportionDroppedFramesPerTrialDuringPulse = [];
-    proportionDroppedFramesPerTrialOutsidePulse = [];
-    for stimulus = 1:length(stimuli)
-        for contrast = 1:length(contrasts)
-            for tt = 1:size(trialStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]), 1)
-                proportionDroppedFramesPerTrial(end+1) = sum(isnan(trialStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})])(tt,:)))/length(trialStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})])(tt,:));
-                proportionDroppedFramesPerTrialDuringPulse(end+1) = sum(isnan(trialStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})])(tt,pulseOnsetIndex:pulseOffsetIndex)))/length(trialStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})])(tt,pulseOnsetIndex:pulseOffsetIndex));
-                proportionDroppedFramesPerTrialOutsidePulse(end+1) = sum(isnan(trialStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})])(tt,[1:pulseOnsetIndex, pulseOffsetIndex:end])))/length(trialStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})])(tt,[1:pulseOnsetIndex, pulseOffsetIndex:end]));
-                
-            end
-        end
-    end
-    
-    
-    
-    % stash these results
-    if strcmp(group, 'mwa') || strcmp(group, 'mwoa')
-        pooledNumberGoodTrials.CombinedMigraineurs(end+1) = numberGoodTrials;
-        pooledProportionGoodTrials.CombinedMigraineurs(end+1) = proportionGoodTrials;
-        
-        pooledTotalDroppedFrames.CombinedMigraineurs(end+1) = nanmean(proportionDroppedFramesPerTrial);
-        pooledDroppedFramesDuringPulse.CombinedMigraineurs(end+1) = nanmean(proportionDroppedFramesPerTrialDuringPulse);
-        pooledDroppedFramesOutsidePulse.CombinedMigraineurs(end+1) = nanmean(proportionDroppedFramesPerTrialOutsidePulse);
-        
-    elseif strcmp(group, 'c')
-        
-        pooledNumberGoodTrials.Controls(end+1) = numberGoodTrials;
-        pooledProportionGoodTrials.Controls(end+1) = proportionGoodTrials;
-        
-        pooledTotalDroppedFrames.Controls(end+1) = nanmean(proportionDroppedFramesPerTrial);
-        pooledDroppedFramesDuringPulse.Controls(end+1) = nanmean(proportionDroppedFramesPerTrialDuringPulse);
-        pooledDroppedFramesOutsidePulse.Controls(end+1) = nanmean(proportionDroppedFramesPerTrialOutsidePulse);
-    end
-    
-    
 end
 
-%% Summarize analyses
-fprintf('<strong>*** Result of good trial analyses ***</strong>\n')
-fprintf('\tMedian number of good trials included across migraine patients: %.1f\n', median(pooledNumberGoodTrials.CombinedMigraineurs));
-fprintf('\tMedian number of good trials included across controls: %.1f\n\n', median(pooledNumberGoodTrials.Controls));
-fprintf('\tMedian proportion of trials included across migraine patients: %.3f\n', median(pooledProportionGoodTrials.CombinedMigraineurs));
-fprintf('\tMedian proportion of trials included across controls: %.3f\n\n', median(pooledProportionGoodTrials.Controls));
 
-plotFig = figure; hold on;
-data = [[pooledNumberGoodTrials.Controls, nan(1,20)]; pooledNumberGoodTrials.CombinedMigraineurs; ];
-categoryIdx = [zeros(1,40), ones(1,40)];
-plotSpread(data', 'categoryIdx', categoryIdx, 'categoryColors', {'k', 'r'})
-plot(1, median(pooledNumberGoodTrials.Controls), '*', 'MarkerSize', 14, 'Color', 'k');
-plot(2, median(pooledNumberGoodTrials.CombinedMigraineurs), '*', 'MarkerSize', 14, 'Color', 'r');
-xticklabels({'Controls', 'Migraineurs'})
-ylabel('Number of Trials')
-title('Total number of included trials')
-export_fig(plotFig, fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', 'pupil/supplementaryAnalyses', 'numberOfIncludedTrials.pdf'));
+%% Loop over subjects
+for ss = 1:length(subjectIDs)
+    % load trialStruct with trialInfoStruct
+    load(fullfile(getpref('melSquintAnalysis', 'melaProcessingPath'), 'Experiments/OLApproach_Squint/SquintToPulse/DataFiles', subjectIDs{ss}, 'trialStruct_radiusSmoothed_droppedFramesAnalysis.mat'));
+    
+    groupLabel = linkMELAIDToGroup(subjectIDs{ss});
+    
+    if strcmp(groupLabel, 'c')
+        groupLabel = 'controls';
+    end
+    
+    
+    for stimulus = 1:length(stimuli)
+        for contrast = 1:length(contrasts)
+            
+            nTrials = length(trialInfoStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]));
+            droppedFramesForStimulusType = [];
+            for tt = 1:nTrials
+                
+                if strcmp(p.Results.range, 'pulse')
+                    rangeIndices = [trialInfoStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]){tt}.pulseOnsetIndex, trialInfoStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]){tt}.pulseOffsetIndex];
+                elseif strcmp(p.Results.range, 'wholeTrial')
+                    rangeIndices = [1 length(trialInfoStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]){tt}.originalTimebase)];
+                end
+                
+                if strcmp(p.Results.whichBadFrames, 'blinks')
+                    blinkFrames = find((trialInfoStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]){tt}.blinkIndices>=rangeIndices(1)) & (trialInfoStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]){tt}.blinkIndices<=rangeIndices(2)));
+                    droppedFrames = blinkFrames;
+                else
+                    blinkFrames = find((trialInfoStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]){tt}.blinkIndices>=rangeIndices(1)) & (trialInfoStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]){tt}.blinkIndices<=rangeIndices(2)));
+                    duplicateFrames = find((trialInfoStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]){tt}.duplicateFrameIndices>=rangeIndices(1)) & (trialInfoStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]){tt}.duplicateFrameIndices<=rangeIndices(2)));
+                    poorFitFrames = find((trialInfoStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]){tt}.poorFitFrameIndices>=rangeIndices(1)) & (trialInfoStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]){tt}.poorFitFrameIndices<=rangeIndices(2)));
+                    initialNaNFrames = find((trialInfoStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]){tt}.initialNaNFrames>=rangeIndices(1)) & (trialInfoStruct.(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]){tt}.initialNaNFrames<=rangeIndices(2)));
+                    droppedFrames = [blinkFrames, duplicateFrames', poorFitFrames', initialNaNFrames'];
+                    droppedFrames = unique(droppedFrames);
+                end
+                droppedFramesTrialStruct.(groupLabel).(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})])(tt) = length(droppedFrames);
+                
+                droppedFramesForStimulusType = [length(droppedFrames), droppedFramesForStimulusType];
 
-plotFig = figure; hold on;
-data = [[pooledProportionGoodTrials.Controls, nan(1,20)]; pooledProportionGoodTrials.CombinedMigraineurs; ];
-categoryIdx = [zeros(1,40), ones(1,40)];
-plotSpread(data', 'categoryIdx', categoryIdx, 'categoryColors', {'k', 'r'})
-plot(1, median(pooledProportionGoodTrials.Controls), '*', 'MarkerSize', 14, 'Color', 'k');
-plot(2, median(pooledProportionGoodTrials.CombinedMigraineurs), '*', 'MarkerSize', 14, 'Color', 'r');
-xticklabels({'Controls', 'Migraineurs'})
-ylabel('Proportion of Trials Included')
-title('Proportion of Trials Included')
-export_fig(plotFig, fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', 'pupil/supplementaryAnalyses', 'proportionOfTrialsIncluded.pdf'));
+
+            end
+            
+            droppedFramesMeanStruct.(groupLabel).(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})])(end+1) = nanmean(droppedFramesForStimulusType);
+            
+        end
+    end
+end
+
+%% Do some summary plotting
+close all;
+droppedFramesMeanStructForPlotting.Controls = droppedFramesMeanStruct.controls;
+droppedFramesMeanStructForPlotting.MwA = droppedFramesMeanStruct.mwa;
+droppedFramesMeanStructForPlotting.MwoA = droppedFramesMeanStruct.mwoa;
+
+savePath = fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', 'pupil', 'droppedFramesAnalysis');
+if ~exist(savePath)
+    mkdir(savePath);
+end
+
+if strcmp(p.Results.whichBadFrames, 'blinks')
+    saveName = ['droppedFramesAnalysis_blinksOnly_', p.Results.range];
+else
+    saveName = ['droppedFramesAnalysis_allBadFrames_', p.Results.range];
+
+end
+
+if strcmp(p.Results.range, 'pulse')
+    yLims = [0 90];
+    
+elseif strcmp(p.Results.range, 'wholeTrial')
+    yLims = [0 500];
+end
+
+plotSpreadResults(droppedFramesMeanStructForPlotting, 'yLims', yLims, 'saveName', fullfile(savePath, saveName))
 
 
+[ pupilStruct ] = loadPupilResponses;
+for group = 1:length(groups)
+    plotFig = figure;
+    for stimulus = 1:length(stimuli)
+        for contrast = 1:length(contrasts)
+            subplot(3,3, (((stimulus-1)*3)+contrast)); hold on
+            x = droppedFramesMeanStruct.(groups{group}).(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]);
+            y = pupilStruct.AUC.(groups{group}).(stimuli{stimulus}).(['Contrast', num2str(contrasts{contrast})]);
+            plot(x, y, 'o', 'Color', groupColors{group});
+            
+            coeffs = polyfit(x, y, 1);
+            fittedX = linspace(min(x), max(x), 200);
+            fittedY = polyval(coeffs, fittedX);
+            ax.ax1 = plot(fittedX, fittedY, 'LineWidth', 1, 'Color', 'k');
+            pearsonCorrelation = corr2(x, y);
+            
+            title([stimuli{stimulus}, ' Contrast ', num2str(contrasts{contrast}), '%, r = ' num2str(pearsonCorrelation)]);
 
+            
+            xlabel('Mean Dropped Frames');
+            ylabel('Pupil Constriction AUC');
+            ylim([0 450]);
+            
+            if strcmp(p.Results.range, 'pulse')
+                xlim([0 100]);
+            else
+                xlim([0 500]);
+            end
+            
+        end
+    end
+    set(plotFig, 'Position', [88 180 1152 798]);
+    if strcmp(p.Results.whichBadFrames, 'blinks')
+        export_fig(plotFig, fullfile(savePath, ['droppedFrames_blinks_', p.Results.range, 'XAUC_', groups{group}, '.pdf']));
+    else
+        export_fig(plotFig, fullfile(savePath, ['droppedFrames_allBadFrames_', p.Results.range, 'XAUC_', groups{group}, '.pdf']));
+    end
 
+end
 
-fprintf('<strong>*** Result of dropped frames analyses ***</strong>\n')
-fprintf('\tMedian proportion of total dropped frames across migraine patients: %.3f\n', median(pooledTotalDroppedFrames.CombinedMigraineurs));
-fprintf('\tMedian proportion of total dropped frames across controls: %.3f\n\n', median(pooledTotalDroppedFrames.Controls));
-fprintf('\tMedian proportion of dropped frames during the pulse across migraine patients: %.3f\n', median(pooledDroppedFramesDuringPulse.CombinedMigraineurs));
-fprintf('\tMedian proportion of dropped frames during the pulse across controls: %.3f\n\n', median(pooledDroppedFramesDuringPulse.Controls));
+plotFig = figure;
+for group = 1:length(groups)
+    
+    for stimulus = 1:length(stimuli)
+            subplot(1,3, stimulus); hold on
+            x = [median(droppedFramesMeanStruct.(groups{group}).(stimuli{stimulus}).Contrast100), ...
+                median(droppedFramesMeanStruct.(groups{group}).(stimuli{stimulus}).Contrast200), ...
+                median(droppedFramesMeanStruct.(groups{group}).(stimuli{stimulus}).Contrast400)];
+            
+            y = [median(pupilStruct.AUC.(groups{group}).(stimuli{stimulus}).Contrast100), ...
+                median(pupilStruct.AUC.(groups{group}).(stimuli{stimulus}).Contrast200), ...
+                median(pupilStruct.AUC.(groups{group}).(stimuli{stimulus}).Contrast400)];
+            
+            plot(x, y, 'o', 'Color', groupColors{group});
+            
 
-plotFig = figure; hold on;
-data = [[pooledTotalDroppedFrames.Controls, nan(1,20)]; pooledTotalDroppedFrames.CombinedMigraineurs; ];
-categoryIdx = [zeros(1,40), ones(1,40)];
-plotSpread(data', 'categoryIdx', categoryIdx, 'categoryColors', {'k', 'r'})
-plot(1, median(pooledTotalDroppedFrames.Controls), '*', 'MarkerSize', 14, 'Color', 'k');
-plot(2, median(pooledTotalDroppedFrames.CombinedMigraineurs), '*', 'MarkerSize', 14, 'Color', 'r');
-xticklabels({'Controls', 'Migraineurs'})
-ylabel('Proportion of Frames Dropped')
-title('Proportion of Total Frames Dropped')
-export_fig(plotFig, fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', 'pupil/supplementaryAnalyses', 'proportionOfTotalFramesDropped.pdf'));
+            
+            title([stimuli{stimulus}]);
 
-plotFig = figure; hold on;
-data = [[pooledDroppedFramesDuringPulse.Controls, nan(1,20)]; pooledDroppedFramesDuringPulse.CombinedMigraineurs; ];
-categoryIdx = [zeros(1,40), ones(1,40)];
-plotSpread(data', 'categoryIdx', categoryIdx, 'categoryColors', {'k', 'r'})
-plot(1, median(pooledDroppedFramesDuringPulse.Controls), '*', 'MarkerSize', 14, 'Color', 'k');
-plot(2, median(pooledDroppedFramesDuringPulse.CombinedMigraineurs), '*', 'MarkerSize', 14, 'Color', 'r');
-xticklabels({'Controls', 'Migraineurs'})
-ylabel('Proportion of Frames Dropped')
-title('Proportion of Frames Dropped During Pulse')
-export_fig(plotFig, fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', 'pupil/supplementaryAnalyses', 'proportionOfFramesDroppedDuringPulse.pdf'));
+            
+            xlabel('Mean Dropped Frames');
+            ylabel('Pupil Constriction AUC');
+            ylim([0 250]);
+            
+            if strcmp(p.Results.range, 'pulse')
+                xlim([0 20]);
+            else
+                xlim([0 500]);
+            end
+            
+        
+    end
+    set(plotFig, 'Position', [88 180 1152 798]);
+    if strcmp(p.Results.whichBadFrames, 'blinks')
+        export_fig(plotFig, fullfile(savePath, ['droppedFrames_blinks_', p.Results.range, 'XAUC','.pdf']));
+    else
+        export_fig(plotFig, fullfile(savePath, ['droppedFrames_allBadFrames_', p.Results.range, 'XAUC', '.pdf']));
+    end
 
+end
 
 
 
