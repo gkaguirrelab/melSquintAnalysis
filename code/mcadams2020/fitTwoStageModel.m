9379aa9312d9b35e1069acbda87f35e19ad06fce
gkaguirre <gkaguirre@me.com>
2020-03-25 19:06:42 -0400

    Figure save fix
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/mcadams2020/fitTwoStageModel.m
+++ code/mcadams2020/fitTwoStageModel.m
@@ -51,7 +51,7 @@ function [figHandle1, figHandle2, rngSeed] = fitTwoStageModel(varargin)
     % Fit the discomfort data
     [~, figHandle2] = fitTwoStageModel('modality','discomfort','rngSeed',1000);
     % Save figure 2
-    print(figHandle2, '~/Desktop/discomfort_params.pdf', '-dpdf', '-fillpage')
+    saveas(figHandle2, '~/Desktop/discomfort_params.pdf', '-dpdf', '-fillpage')
 %}
 %{
     % Re-fit the discomfort data, locking the first two parameters
@@ -60,13 +60,13 @@ function [figHandle1, figHandle2, rngSeed] = fitTwoStageModel(varargin)
     ub = [0.6323, 1.7488, Inf, 10];
     figHandle1 = fitTwoStageModel('modality','discomfort','x0',x0,'lb',lb,'ub',ub,'rngSeed',1000);
     % Save figure 1
-    print(figHandle1, '~/Desktop/discomfort_fit.pdf', '-dpdf', '-fillpage')
+    saveas(figHandle1, '~/Desktop/discomfort_fit.pdf', '-dpdf', '-fillpage')
 %}
 %{
     % Fit the pupil data
     [~, figHandle2] = fitTwoStageModel('modality','pupil','rngSeed',1000);
     % Save figure 2
-    print(figHandle2,'~/Desktop/pupil_params.pdf')
+    saveas(figHandle2,'~/Desktop/pupil_params.pdf')
 %}
 %{
     % Re-fit the pupil data, locking all parameters
@@ -75,14 +75,14 @@ function [figHandle1, figHandle2, rngSeed] = fitTwoStageModel(varargin)
     ub = [0.4152, 0.8292, 0.1296, -0.1512];
     figHandle1 = fitTwoStageModel('modality','pupil','x0',x0,'lb',lb,'ub',ub,'nBoots',2,'rngSeed',1000);
     % Save figure 1
-    print(figHandle1, '~/Desktop/pupil_fit.pdf', '-dpdf', '-fillpage')
+    saveas(figHandle1, '~/Desktop/pupil_fit.pdf', '-dpdf', '-fillpage')
 %}
 %{
     % Fit the EMG data
     [figHandle1, figHandle2] = fitTwoStageModel('modality','emg','responseMetric', 'normalizedPulseAUC', 'rngSeed',1000);
     % Save figures
-    print(figHandle1, '~/Desktop/emg_fit.pdf', '-dpdf', '-fillpage')
-    print(figHandle2,'~/Desktop/emg_params.pdf')
+    saveas(figHandle1, '~/Desktop/emg_fit.pdf', '-dpdf', '-fillpage')
+    saveas(figHandle2,'~/Desktop/emg_params.pdf')
 %}
 
 
