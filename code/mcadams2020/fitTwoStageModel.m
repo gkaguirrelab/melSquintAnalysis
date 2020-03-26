1fb9acc37c95e1b414041d6a6b7bb1ed7afa7c5a
gkaguirre <gkaguirre@me.com>
2020-03-26 11:56:25 -0400

    Removed examples that are now in separate script
    Also removed the unusued “local contrast” variable
    
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/mcadams2020/fitTwoStageModel.m
+++ code/mcadams2020/fitTwoStageModel.m
@@ -49,40 +49,7 @@ function [figHandle1, figHandle2, rngSeed] = fitTwoStageModel(varargin)
 % Examples:
 %{
     % Fit the discomfort data
-    [~, figHandle2] = fitTwoStageModel('modality','discomfort','rngSeed',1000);
-    % Save figure 2
-    print(figHandle2, '~/Desktop/discomfort_params.pdf', '-dpdf', '-fillpage')
-%}
-%{
-    % Re-fit the discomfort data, locking the first two parameters
-    x0 = [0.6323, 1.7488, 1, 1];
-    lb = [0.6323, 1.7488, 0, -10];
-    ub = [0.6323, 1.7488, Inf, 10];
-    figHandle1 = fitTwoStageModel('modality','discomfort','x0',x0,'lb',lb,'ub',ub,'rngSeed',1000);
-    % Save figure 1
-    print(figHandle1, '~/Desktop/discomfort_fit.pdf', '-dpdf', '-fillpage')
-%}
-%{
-    % Fit the pupil data
-    [~, figHandle2] = fitTwoStageModel('modality','pupil','rngSeed',1000);
-    % Save figure 2
-    print(figHandle2,'~/Desktop/pupil_params.pdf', '-dpdf', '-fillpage')
-%}
-%{
-    % Re-fit the pupil data, locking all parameters
-    x0 = [0.4152, 0.8292, 0.1296, -0.1512];
-    lb = [0.4152, 0.8292, 0.1296, -0.1512];
-    ub = [0.4152, 0.8292, 0.1296, -0.1512];
-    figHandle1 = fitTwoStageModel('modality','pupil','x0',x0,'lb',lb,'ub',ub,'nBoots',2,'rngSeed',1000);
-    % Save figure 1
-    print(figHandle1, '~/Desktop/pupil_fit.pdf', '-dpdf', '-fillpage')
-%}
-%{
-    % Fit the EMG data
-    [figHandle1, figHandle2] = fitTwoStageModel('modality','emg','responseMetric', 'normalizedPulseAUC', 'rngSeed',1000);
-    % Save figures
-    print(figHandle1, '~/Desktop/emg_fit.pdf', '-dpdf', '-fillpage')
-    print(figHandle2, '~/Desktop/emg_params.pdf', '-dpdf', '-fillpage')
+    fitTwoStageModel('modality','discomfort','rngSeed',1000);
 %}
 
 
@@ -152,7 +119,7 @@ switch modality
     case 'discomfort'
         
         % Load the data
-        [resultsStruct, ~, MelContrastByStimulus, LMSContrastByStimulus, localContrastByStimulus ] = loadDiscomfortRatings();
+        [resultsStruct, ~, MelContrastByStimulus, LMSContrastByStimulus ] = loadDiscomfortRatings();
         
         % Bounds for the parameters
         if isempty(p.Results.x0)
@@ -184,7 +151,7 @@ switch modality
     case 'pupil'
         
         % Load the data
-        [resultsStruct, ~, MelContrastByStimulus, LMSContrastByStimulus, localContrastByStimulus ] = loadPupilResponses();
+        [resultsStruct, ~, MelContrastByStimulus, LMSContrastByStimulus ] = loadPupilResponses();
         resultsStruct = resultsStruct.(responseMetric);
         
         % Bounds for the parameters
@@ -217,7 +184,7 @@ switch modality
     case 'emg'
         
         % Load the data
-        [resultsStruct, ~, MelContrastByStimulus, LMSContrastByStimulus, localContrastByStimulus ] = loadEMG();
+        [resultsStruct, ~, MelContrastByStimulus, LMSContrastByStimulus ] = loadEMG();
         resultsStruct = resultsStruct.(responseMetric);
         
         % Bounds for the parameters
