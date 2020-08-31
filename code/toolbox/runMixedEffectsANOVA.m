976b3236696d06f867749a1807de402320cbfb74
Harrison McAdams <harrison.mcadams@gmail.com>
2020-08-31 12:59:34 -0400

    added ability to look at 400% responses

--- code/toolbox/runMixedEffectsANOVA.m
+++ code/toolbox/runMixedEffectsANOVA.m
@@ -1,13 +1,50 @@
-function runMixedEffectsANOVA(responseModality, responseMetric)
+function runMixedEffectsANOVA(responseModality, linearMetric)
+
+
+stimuli = {'Melanopsin', 'LightFlux', 'LMS'};
+groups = {'controls', 'mwa', 'mwoa'};
 
 %% get model fit params
 
-if strcmp(responseModality, 'pupil')
-    [slope, intercept, meanRating] = fitLineToResponseModality(responseModality, 'makePlots', false, 'makeCSV', false, 'responseMetric', 'AUC');
-elseif strcmp(responseModality, 'emg')
-    [slope, intercept, meanRating] = fitLineToResponseModality(responseModality, 'makePlots', false, 'makeCSV', false, 'responseMetric', 'normalizedPulseAUC');
-else 
-    [slope, intercept, meanRating] = fitLineToResponseModality(responseModality, 'makePlots', false, 'makeCSV', false);
+if ~strcmp(linearMetric, '400%')
+    if strcmp(responseModality, 'pupil')
+        responseMetric = 'AUC';
+        [slope, intercept, meanRating] = fitLineToResponseModality(responseModality, 'makePlots', false, 'makeCSV', false, 'responseMetric', responseMetric);
+    elseif strcmp(responseModality, 'emg')
+        responseMetric = 'normalizedPulseAUC';
+        [slope, intercept, meanRating] = fitLineToResponseModality(responseModality, 'makePlots', false, 'makeCSV', false, 'responseMetric', responseMetric);
+    elseif strcmp(responseModality, 'blinks') || strcmp(responseModality, 'droppedFrames')
+        responseModality = 'droppedFrames';
+        responseMetric = '';
+        [slope, intercept, meanRating] = fitLineToResponseModality(responseModality, 'makePlots', false, 'makeCSV', false, 'responseMetric', []);
+    else
+        
+        [slope, intercept, meanRating] = fitLineToResponseModality(responseModality, 'makePlots', false, 'makeCSV', false);
+    end
+else
+    if strcmp(responseModality, 'emg')
+        responseMetric = 'normalizedPulseAUC';
+        resultStruct = loadEMG;
+        
+        for stimulus = 1:length(stimuli)
+            for group = 1:length(groups)
+                result.(groups{group}).(stimuli{stimulus}) = resultStruct.(responseMetric).(groups{group}).(stimuli{stimulus}).Contrast400;
+            end
+        end
+    elseif strcmp(responseModality, 'droppedFrames')
+        responseMetric = '';
+        resultStruct = loadBlinks;
+        
+        for stimulus = 1:length(stimuli)
+            for group = 1:length(groups)
+                result.(groups{group}).(stimuli{stimulus}) = resultStruct.(groups{group}).(stimuli{stimulus}).Contrast400;
+            end
+        end
+    end
+    
+
+    
+    
 end
 %% Create the design matrix
 %     - first column  (i.e., X(:,1)) : all dependent variable values
@@ -18,16 +55,15 @@ end
 %     - fourth column (i.e., X(:,4)) : subject codes (ranging from 1:N where N is the total number of subjects)
 
 % grab the result of choice
-if strcmp(responseMetric, 'slope')
+if strcmp(linearMetric, 'slope')
     result = slope;
-elseif strcmp(responseMetric, 'intercept')
+elseif strcmp(linearMetric, 'intercept')
     result = intercept;
-elseif strcmp(responseMetric, 'meanRating')
+elseif strcmp(linearMetric, 'meanRating')
     result = meanRating;
 end
 % First column: all dependent variables:
-stimuli = {'Melanopsin', 'LightFlux', 'LMS'};
-groups = {'controls', 'mwa', 'mwoa'};
+
 for group = 1:length(groups)
     for ss = 1:20
         for stimulus = 1:length(stimuli)
@@ -94,15 +130,22 @@ withinSubjectsFValue = table2array(ranovatbl(1,4));
 withinSubjectspValue = table2array(ranovatbl(1,5));
 interactionFValue = table2array(ranovatbl(2,4));
 interactionpValue = table2array(ranovatbl(2,5));
-fprintf('\n<strong>From Matlab functions: </strong>\n');
-fprintf('The effect of stimulus: F-value of %4.3f, p-value of %4.5f\n', withinSubjectsFValue, withinSubjectspValue);
-fprintf('The effect of interaction: F-value of %4.3f, p-value of %4.5f\n', interactionFValue, interactionpValue);
 
+tableSavePath = fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', responseModality, responseMetric);
+%fprintf('\n<strong>From Matlab functions: </strong>\n');
+%fprintf('The effect of stimulus: F-value of %4.3f, p-value of %4.5f\n', withinSubjectsFValue, withinSubjectspValue);
+%fprintf('The effect of interaction: F-value of %4.3f, p-value of %4.5f\n', interactionFValue, interactionpValue);
 % to find the between subject effects:
 anovatbl = anova(rm);
 betweenSubjectsFValue = table2array(anovatbl(2,6));
 betweenSubjectspValue = table2array(anovatbl(2,7));
-fprintf('The effect of group: F-value of %4.3f, p-value of %4.5f\n', betweenSubjectsFValue, betweenSubjectspValue);
+%fprintf('The effect of group: F-value of %4.3f, p-value of %4.5f\n', betweenSubjectsFValue, betweenSubjectspValue);
+
+system(['echo ', sprintf('Mixed effects ANOVA results for %s on %s:', responseModality, linearMetric), ' > "' fullfile(tableSavePath, ['mixedEffectsOverview_', linearMetric, '.txt']), '"']);
+system(['echo ', sprintf('The effect of stimulus: F-value of %4.3f, p-value of %4.5f', withinSubjectsFValue, withinSubjectspValue), ' >> "' fullfile(tableSavePath, ['mixedEffectsOverview_', linearMetric, '.txt']), '"']);
+system(['echo ', sprintf('The effect of group: F-value of %4.3f, p-value of %4.5f', betweenSubjectsFValue, betweenSubjectspValue), ' >> "' fullfile(tableSavePath, ['mixedEffectsOverview_', linearMetric, '.txt']), '"']);
+system(['echo ', sprintf('The effect of interaction: F-value of %4.3f, p-value of %4.5f', interactionFValue, interactionpValue), ' >> "' fullfile(tableSavePath, ['mixedEffectsOverview_', linearMetric, '.txt']), '"']);
+
 
 %% make summary table:
 posthocTable = multcompare(rm,'Stimulus', 'By', 'Group');
@@ -161,7 +204,7 @@ end
 
 stimulusByGroupTable = array2table(stimulusByGroupTable);
 stimulusByGroupTable.Properties.VariableNames = {'Group', 'Comparison', 'Difference', 't_Statistic', 'p_Value', 'CI_95'};
-writetable(stimulusByGroupTable, fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', responseModality, ['stimulusByGroup_', responseMetric, '_postHoc.csv']))
+writetable(stimulusByGroupTable, fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', responseModality, responseMetric, ['stimulusByGroup_', linearMetric, '_postHoc.csv']))
 
 
 % group by stimulus
@@ -221,6 +264,53 @@ end
 
 groupByStimulusTable = array2table(groupByStimulusTable);
 groupByStimulusTable.Properties.VariableNames = {'Stimulus', 'Comparison', 'Difference', 't_Statistic', 'p_Value', 'CI_95'};
-writetable(groupByStimulusTable, fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', responseModality, ['groupBystimulus_', responseMetric, '_postHoc.csv']))
+writetable(groupByStimulusTable, fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', responseModality, responseMetric, ['groupBystimulus_', linearMetric, '_postHoc.csv']))
+
+
+%% Summary table, collapsing across stimuli
+controlValues = table2array(rm.BetweenDesign(1:20,1:3));
+mwaValues = table2array(rm.BetweenDesign(21:40,1:3));
+mwoaValues = table2array(rm.BetweenDesign(41:60,1:3));
+postHocTable = multcompare(rm, 'Group');
+
+groupTable{1,1} = 'MwA - HAf';
+groupTable{1,2} = sprintf('%4.3f - %4.3f = %4.3f', mean(mwaValues(:)), mean(controlValues(:)), mean(mwaValues(:)) - mean(controlValues(:)));
+groupTable{1,3} = sprintf('%4.3f', table2array(postHocTable(3,3))/table2array(postHocTable(3,4)));
+
+if table2array(postHocTable(1,5)) < 0.05
+    
+    groupTable{1,4} = sprintf('*%0.3e', table2array(postHocTable(1,5)));
+else
+    groupTable{1,4} = sprintf('%0.3e', table2array(postHocTable(1,5)));
+    
+end
+groupTable{1,5} = sprintf('%4.3f - %4.3f', table2array(postHocTable(3,6)), table2array(postHocTable(3,7)));
+
+groupTable{2,1} = 'MwoA - HAf';
+groupTable{2,2} = sprintf('%4.3f - %4.3f = %4.3f', mean(mwoaValues(:)), mean(controlValues(:)), mean(mwoaValues(:)) - mean(controlValues(:)));
+groupTable{2,3} = sprintf('%4.3f', table2array(postHocTable(2,3))/table2array(postHocTable(2,4)));
+
+if table2array(postHocTable(2,5)) < 0.05
+    groupTable{2,4} = sprintf('*%0.3e', table2array(postHocTable(2,5)));
+else
+    groupTable{2,4} = sprintf('%0.3e', table2array(postHocTable(2,5)));
+    
+end
+groupTable{2,5} = sprintf('%4.3f - %4.3f', table2array(postHocTable(5,6)), table2array(postHocTable(5,7)));
+
+groupTable{3,1} = 'MwoA - MwA';
+groupTable{3,2} = sprintf('%4.3f - %4.3f = %4.3f', mean(mwoaValues(:)), mean(mwaValues(:)), mean(mwoaValues(:)) - mean(mwaValues(:)));
+groupTable{3,3} = sprintf('%4.3f', table2array(postHocTable(6,3))/table2array(postHocTable(6,4)));
+
+if table2array(postHocTable(6,5)) < 0.05
+    groupTable{3,4} = sprintf('*%0.3e', table2array(postHocTable(6,5)));
+else
+    groupTable{3,4} = sprintf('%0.3e', table2array(postHocTable(6,5)));
+end
+groupTable{3,5} = sprintf('%4.3f - %4.3f', table2array(postHocTable(6,6)), table2array(postHocTable(6,7)));
+
+groupTable = array2table(groupTable);
+groupTable.Properties.VariableNames = {'Comparison', 'Difference', 't_Statistic', 'p_Value', 'CI_95'};
+writetable(groupTable, fullfile(getpref('melSquintAnalysis', 'melaAnalysisPath'), 'melSquintAnalysis', responseModality, responseMetric, ['group_', linearMetric, '_postHoc.csv']))
 
 end
